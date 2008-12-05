922afbb53e7da1f519b71f2413bd3e6f3fd7b1c5
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2008-12-05 19:00:02 +0000

    initial commit - import from CU repository
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@66 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ test/src/org/cleartk/classifier/mallet/FactoryTests.java
@@ -0,0 +1,127 @@
+ /** 
+ * Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+*/
+package org.cleartk.classifier.mallet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.cleartk.classifier.mallet.factory.C45TrainerFactory;
+import org.cleartk.classifier.mallet.factory.ClassifierTrainerFactory;
+import org.cleartk.classifier.mallet.factory.MCMaxEntTrainerFactory;
+import org.cleartk.classifier.mallet.factory.MaxEntTrainerFactory;
+import org.cleartk.classifier.mallet.factory.NaiveBayesTrainerFactory;
+import org.junit.Test;
+
+import cc.mallet.classify.C45Trainer;
+import cc.mallet.classify.NaiveBayesTrainer;
+
+/**
+ * <br>Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * <br>All rights reserved.
+
+ * 
+ * @author Philip Ogren
+ */
+
+public class FactoryTests {
+
+	@Test
+	public void testC45TrainerFactory() {
+		C45TrainerFactory factory = new C45TrainerFactory();
+		C45Trainer trainer = (C45Trainer) factory.createTrainer(new String[] { "--depthLimited", "true", "--doPruning",
+				"false", "--maxDepth", "6", "--minNumInsts", "3" });
+
+		assertTrue(trainer.getDepthLimited());
+		assertFalse(trainer.getDoPruning());
+		assertEquals(6, trainer.getMaxDepth());
+		assertEquals(3, trainer.getMinNumInsts());
+
+		testBadConfig(factory, new String[] { "--depthLimited", "--doPruning", "false", "--maxDepth", "6",
+				"--minNumInsts", "3" });
+
+		testBadConfig(factory, new String[] { "--depthimited", "true" });
+		testBadConfig(factory, new String[] { "--numIterations" });
+
+		assertTrue(factory.getClass().getName().endsWith("TrainerFactory"));
+	}
+
+	@Test
+	public void testMaxentTrainerFactory() {
+		MaxEntTrainerFactory factory = new MaxEntTrainerFactory();
+		factory.createTrainer(new String[] { "--numIterations", "200", "--gaussianPriorVariance", "0.552" });
+
+		testBadConfig(factory, new String[] { "--depthimited", "true" });
+		testBadConfig(factory, new String[] { "--numIterations", "true" });
+		testBadConfig(factory, new String[] { "--numIterations", "-5" });
+		testBadConfig(factory, new String[] { "--numIterations" });
+
+		assertTrue(factory.getClass().getName().endsWith("TrainerFactory"));
+	}
+
+	@Test
+	public void testMCMaxentTrainerFactory() {
+		MCMaxEntTrainerFactory factory = new MCMaxEntTrainerFactory();
+		factory.createTrainer(new String[] { "--useHyperbolicPrior", "true",
+				"--gaussianPriorVariance", "0.552",
+				"--hyperbolicPriorSlope", "0.3",
+				"--hyperbolicPriorSharpness", "15.0",
+				"--numIterations", "20"});
+
+		testBadConfig(factory, new String[] { "--hyperbolicPriorSlope", "false" });
+		testBadConfig(factory, new String[] { "--numIterations" });
+		testBadConfig(factory, new String[] { "--numItertions", "true" });
+
+		assertTrue(factory.getClass().getName().endsWith("TrainerFactory"));
+	}
+
+	@Test
+	public void testNaiveBayesTrainerFactory() {
+		NaiveBayesTrainerFactory factory = new NaiveBayesTrainerFactory();
+		NaiveBayesTrainer trainer = (NaiveBayesTrainer) factory.createTrainer(new String[] { "--docLengthNormalization", "15.0"});
+
+		assertEquals(15.0, trainer.getDocLengthNormalization(), 0.001);
+		
+		testBadConfig(factory, new String[] { "--hyperbolicPriorSlope", "false" });
+		testBadConfig(factory, new String[] { "--docLengthNormalization"});
+		testBadConfig(factory, new String[] { "--docLengthNormalization", "15.0", "--useHyperbolicPrior", "true" });
+
+		assertTrue(factory.getClass().getName().endsWith("TrainerFactory"));
+	}
+
+	@SuppressWarnings("unchecked")
+	private void testBadConfig(ClassifierTrainerFactory factory, String... args) {
+		IllegalArgumentException exception = null;
+		try {
+			factory.createTrainer(args);
+		}
+		catch (IllegalArgumentException iae) {
+			exception = iae;
+		}
+		assertNotNull(exception);
+
+	}
+
+}
---
+++ test/src/org/cleartk/classifier/mallet/RunMalletTests.java
@@ -0,0 +1,219 @@
+ /** 
+ * Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+*/
+package org.cleartk.classifier.mallet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.cleartk.classifier.ClassifierFactory;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.Instance;
+import org.cleartk.classifier.Train;
+import org.cleartk.classifier.encoder.factory.ContextValueEncoderFactory;
+import org.cleartk.classifier.mallet.MalletClassifier;
+import org.cleartk.classifier.mallet.MalletDataWriter;
+import org.cleartk.util.EmptyAnnotator;
+import org.cleartk.util.TestsUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import cc.mallet.types.FeatureVector;
+/**
+ * <br>Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * <br>All rights reserved.
+
+* 
+* @author Philip Ogren
+*/
+
+public class RunMalletTests {
+	Random random;
+	
+	@Before
+	public void setUp() {
+		random = new Random(System.currentTimeMillis());
+	}
+	
+	@Test
+	public void runTest1() throws Exception {
+		String outputDirectory = "test/data/mallet/run-test-1"; 
+		AnalysisEngine engine = TestsUtil.getAnalysisEngine(EmptyAnnotator.class, 
+				TestsUtil.getTypeSystem("desc/TypeSystem.xml"), 
+				MalletDataWriter.PARAM_ANNOTATION_HANDLER,
+				"org.cleartk.example.ExamplePOSAnnotationHandler",
+				MalletDataWriter.PARAM_OUTPUT_DIRECTORY,
+				outputDirectory);
+		
+		MalletDataWriter dataWriter = new MalletDataWriter();
+		dataWriter.initialize(engine.getUimaContext());
+		
+		
+		List<Instance<String>> instances = new ArrayList<Instance<String>>();
+		
+		
+		for(int i=0; i<1000; i++)
+			instances.add(generateInstance());
+			
+		dataWriter.consumeAll(instances);
+		dataWriter.collectionProcessComplete();
+		engine.collectionProcessComplete();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(outputDirectory+"/training-data.mallet"));
+		reader.readLine();
+		reader.close();
+
+		IllegalArgumentException exception = null;
+		try {
+			Train.main(new String[] {outputDirectory, "asdf"});
+		} catch(IllegalArgumentException iae) {
+			exception = iae;
+		}
+		assertNotNull(exception);
+		
+		exception = null;
+		try {
+			Train.main(new String[] {outputDirectory, "MaxEnt", "10", "asdf"});
+		} catch(IllegalArgumentException iae) {
+			exception = iae;
+		}
+		assertNotNull(exception);
+		
+		TestsUtil.HideOutput hider = new TestsUtil.HideOutput();
+		Train.main(new String[] {outputDirectory, "C45"});
+		hider.restoreOutput();
+
+		MalletClassifier classifier = (MalletClassifier) ClassifierFactory.readFromJar(outputDirectory+"/model.jar");
+		assertFalse(classifier.isSequential());
+		
+		List<List<Feature>> testFeatures = new ArrayList<List<Feature>>();
+		
+		//they aren't the same because MaxEnt can't discriminate on a single feature
+		Instance<String> testInstance = new Instance<String>();
+		testInstance.add(new Feature("hello", random.nextInt(1000)+1000));
+		testFeatures.add(testInstance.getFeatures());
+		String outcome = classifier.classify(testInstance.getFeatures());
+		assertEquals("A", outcome);
+
+		testInstance = new Instance<String>();
+		testInstance.add(new Feature("hello", 95));
+		testInstance.add(new Feature("goodbye", 95));
+		testFeatures.add(testInstance.getFeatures());
+		outcome = classifier.classify(testInstance.getFeatures());
+		assertEquals("B", outcome);
+		
+		cc.mallet.types.Instance malletInstance = classifier.toInstance(testInstance.getFeatures());
+		FeatureVector fv = (FeatureVector) malletInstance.getData();
+		assertEquals(95.0, fv.value("hello"), 0.001);
+
+		List<String> outcomes = classifier.classifySequence(testFeatures);
+		assertEquals(2, outcomes.size());
+		assertEquals("A", outcomes.get(0));
+		assertEquals("B", outcomes.get(1));
+	}
+
+	private Instance<String> generateInstance(){
+		Instance<String> instance = new Instance<String>();
+		
+		int outcome = random.nextInt(2);
+		if(outcome == 0) {
+			instance.setOutcome("A");
+			instance.add(new Feature("hello", random.nextInt(1000)+1000));
+		} else {
+			instance.setOutcome("B");
+			instance.add(new Feature("hello", random.nextInt(100)));
+		}
+		return instance;
+	}
+
+	@Test
+	public void runTest2() throws Exception {
+		String outputDirectory = "test/data/mallet/run-test-2"; 
+		AnalysisEngine engine = TestsUtil.getAnalysisEngine(EmptyAnnotator.class, 
+				TestsUtil.getTypeSystem("desc/TypeSystem.xml"), 
+				MalletDataWriter.PARAM_ANNOTATION_HANDLER,
+				"org.cleartk.example.ExamplePOSAnnotationHandler",
+				MalletDataWriter.PARAM_OUTPUT_DIRECTORY,
+				outputDirectory,
+				ContextValueEncoderFactory.PARAM_COMPRESS,
+				true);
+		
+		MalletDataWriter dataWriter = new MalletDataWriter();
+		dataWriter.initialize(engine.getUimaContext());
+		
+		List<Instance<String>> instances = new ArrayList<Instance<String>>();
+		
+		
+		for(int i=0; i<1000; i++)
+			instances.add(generateInstance());
+			
+		dataWriter.consumeAll(instances);
+		dataWriter.collectionProcessComplete();
+		engine.collectionProcessComplete();
+
+		BufferedReader reader = new BufferedReader(new FileReader(outputDirectory+"/training-data.mallet"));
+		reader.readLine();
+		reader.close();
+
+		TestsUtil.HideOutput hider = new TestsUtil.HideOutput();
+		Train.main(new String[] {outputDirectory, "C45"});
+		hider.restoreOutput();
+		
+		MalletClassifier classifier = (MalletClassifier) ClassifierFactory.readFromJar(outputDirectory+"/model.jar");
+		assertFalse(classifier.isSequential());
+		
+		List<List<Feature>> testFeatures = new ArrayList<List<Feature>>();
+		
+		//they aren't the same because MaxEnt can't discriminate on a single feature
+		Instance<String> testInstance = new Instance<String>();
+		testInstance.add(new Feature("hello", random.nextInt(1000)+1000));
+		testFeatures.add(testInstance.getFeatures());
+		String outcome = classifier.classify(testInstance.getFeatures());
+		assertEquals("A", outcome);
+
+		testInstance = new Instance<String>();
+		testInstance.add(new Feature("hello", 95));
+		testInstance.add(new Feature("goodbye", 95));
+		testFeatures.add(testInstance.getFeatures());
+		outcome = classifier.classify(testInstance.getFeatures());
+		assertEquals("B", outcome);
+		
+		cc.mallet.types.Instance malletInstance = classifier.toInstance(testInstance.getFeatures());
+		FeatureVector fv = (FeatureVector) malletInstance.getData();
+		assertEquals(95.0, fv.value("0"), 0.001);
+
+		List<String> outcomes = classifier.classifySequence(testFeatures);
+		assertEquals(2, outcomes.size());
+		assertEquals("A", outcomes.get(0));
+		assertEquals("B", outcomes.get(1));
+
+	}
+}
