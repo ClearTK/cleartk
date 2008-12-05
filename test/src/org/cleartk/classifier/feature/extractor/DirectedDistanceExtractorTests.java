18119c72d02a903a0d6ea8444be734218beea38d
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2008-12-05 18:42:57 +0000

    initial commit - import from CU repository
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@61 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ test/src/org/cleartk/classifier/feature/extractor/DirectedDistanceExtractorTests.java
@@ -0,0 +1,106 @@
+ /** 
+ * Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+*/
+package org.cleartk.classifier.feature.extractor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.feature.extractor.DirectedDistanceExtractor;
+import org.cleartk.type.Token;
+import org.cleartk.util.AnnotationRetrieval;
+import org.cleartk.util.EmptyAnnotator;
+import org.cleartk.util.TestsUtil;
+import org.junit.Test;
+
+
+/**
+ * <br>Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * <br>All rights reserved.
+
+ *
+ * @author Philip Ogren
+ */
+
+public class DirectedDistanceExtractorTests {
+
+	@Test
+	public void test1() throws Exception {
+		AnalysisEngine engine = TestsUtil.getAnalysisEngine(EmptyAnnotator.class, TestsUtil.getTypeSystem("desc/TypeSystem.xml"));
+		JCas jCas = engine.newJCas();
+		
+		TestsUtil.createTokens(jCas, "A simple sentence to test the distance of tokens from each other.", "A simple sentence to test the distance of tokens from each other .", null, null);
+		
+				
+		DirectedDistanceExtractor extractor = new DirectedDistanceExtractor(null, Token.class);
+		
+		Token token1 = AnnotationRetrieval.get(jCas, Token.class, 0);
+		Token token2 = AnnotationRetrieval.get(jCas, Token.class, 1);
+		Token token3 = AnnotationRetrieval.get(jCas, Token.class, 2);
+		Token token4 = AnnotationRetrieval.get(jCas, Token.class, 3);
+		Token token5 = AnnotationRetrieval.get(jCas, Token.class, 4);
+		
+		Feature feature = extractor.extract(jCas, token1, token2);
+		assertEquals(1, feature.getValue());
+		feature = extractor.extract(jCas, token2, token1);
+		assertEquals(-1, feature.getValue());
+
+		feature = extractor.extract(jCas, token1, token3);
+		assertEquals(2, feature.getValue());
+		feature = extractor.extract(jCas, token3, token1);
+		assertEquals(-2, feature.getValue());
+
+		feature = extractor.extract(jCas, token1, token5);
+		assertEquals(4, feature.getValue());
+		feature = extractor.extract(jCas, token5, token1);
+		assertEquals(-4, feature.getValue());
+
+		feature = extractor.extract(jCas, token4, token5);
+		assertEquals(1, feature.getValue());
+		feature = extractor.extract(jCas, token5, token4);
+		assertEquals(-1, feature.getValue());
+
+		feature = extractor.extract(jCas, token5, token5);
+		assertEquals(0, feature.getValue());
+		
+		feature = extractor.extract(jCas, new Annotation(jCas, 0,3), token1);
+		assertEquals(0, feature.getValue());
+		feature = extractor.extract(jCas, new Annotation(jCas, 0,3), token2);
+		assertEquals(0, feature.getValue());
+		feature = extractor.extract(jCas, new Annotation(jCas, 0,3), token3);
+		assertEquals(1, feature.getValue());
+		feature = extractor.extract(jCas, new Annotation(jCas, 0,3), token4);
+		assertEquals(2, feature.getValue());
+		feature = extractor.extract(jCas, new Annotation(jCas, 0,3), token5);
+		assertEquals(3, feature.getValue());
+		
+		Annotation annotation = new Annotation(jCas, 64, 65);
+		assertEquals(".", annotation.getCoveredText());
+		feature = extractor.extract(jCas, annotation, token1);
+		assertEquals(-12, feature.getValue());
+
+	}
+}
---
+++ test/src/org/cleartk/classifier/feature/extractor/NGramExtractorTests.java
@@ -0,0 +1,100 @@
+ /** 
+ * Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+*/
+package org.cleartk.classifier.feature.extractor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.jcas.JCas;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.feature.extractor.NGramExtractor;
+import org.cleartk.classifier.feature.extractor.SpannedTextExtractor;
+import org.cleartk.classifier.feature.extractor.TypePathExtractor;
+import org.cleartk.type.Document;
+import org.cleartk.type.Token;
+import org.cleartk.util.DocumentUtil;
+import org.cleartk.util.TestsUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * <br>Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * <br>All rights reserved.
+
+ * 
+ * @author Steven Bethard
+ */
+
+public class NGramExtractorTests {
+
+	@Test
+	public void test() throws UIMAException {
+		JCas jCas = TestsUtil.newJCas();
+		TestsUtil.createTokens(jCas,
+				"She sells seashells by the sea shore",
+				null, 
+				"PRP VBZ NNS IN DT NN NN",
+				null);
+		DocumentUtil.createDocument(jCas, "foo", "bar");
+		Document document = DocumentUtil.getDocument(jCas);
+		
+		SpannedTextExtractor textExtractor = new SpannedTextExtractor();
+		TypePathExtractor posExtractor = new TypePathExtractor(Token.class, "pos");
+		NGramExtractor extractor;
+		List<Feature> features;
+		
+		extractor = new NGramExtractor(2, Token.class, textExtractor);
+		Assert.assertEquals("|", extractor.getValueSeparator());
+		features = extractor.extract(jCas, document);
+		Assert.assertEquals(6, features.size());
+		this.checkFeatures(
+				features, "Ngram_SpannedText_SpannedText",
+				"She|sells", "sells|seashells", "seashells|by",
+				"by|the", "the|sea", "sea|shore");
+		
+		extractor = new NGramExtractor("Foo", 3, Token.class, posExtractor);
+		extractor.setValueSeparator("@");
+		Assert.assertEquals("@", extractor.getValueSeparator());
+		features = extractor.extract(jCas, document);
+		Assert.assertEquals(5, features.size());
+		this.checkFeatures(
+				features, "Foo_Ngram_TypePath_Pos_TypePath_Pos_TypePath_Pos",
+				"PRP@VBZ@NNS", "VBZ@NNS@IN", "NNS@IN@DT", "IN@DT@NN", "DT@NN@NN");
+		
+	}
+	
+	private void checkFeatures(
+			List<Feature> features,
+			String expectedName,
+			Object ... expectedValues) {
+		List<Object> actualValues = new ArrayList<Object>();
+		for (Feature feature: features) {
+			Assert.assertEquals(expectedName, feature.getName());
+			actualValues.add(feature.getValue());
+		}
+		Assert.assertEquals(Arrays.asList(expectedValues), actualValues);
+	}
+}
