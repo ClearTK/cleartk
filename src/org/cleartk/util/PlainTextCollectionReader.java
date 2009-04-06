c3ee27d0f0b37d41b69ef1a6b5579fe1a21ba4aa
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-06 21:48:58 +0000

    - code formatted - renamed parameters to conform to #44 'parameter names should be "fully-qualified"' - renamed PARAM_FILE_NAMES to PARAM_FILE_NAMES_FILES per #79 'change PlainTextCollectionReader.PARAM_FILE_NAMES' and added new param PARAM_FILE_NAMES which allows you to specify a list of file names. - refactored initialization logic that keeps the params PARAM_SUFFIXES, PARAM_FILE_NAMES_FILES, and PARAM_FILE_NAMES get handled correctly (as mutually exclusive).
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@412 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- src/org/cleartk/util/PlainTextCollectionReader.java
+++ src/org/cleartk/util/PlainTextCollectionReader.java
@@ -1,4 +1,4 @@
- /** 
+/** 
  * Copyright (c) 2007-2008, Regents of the University of Colorado 
  * All rights reserved.
  * 
@@ -20,7 +20,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE. 
-*/
+ */
 package org.cleartk.util;
 
 import java.io.File;
@@ -42,10 +42,10 @@ import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
 import org.cleartk.util.io.Files;
 
-
 /**
- * <br>Copyright (c) 2007-2008, Regents of the University of Colorado 
- * <br>All rights reserved.
+ * <br>
+ * Copyright (c) 2007-2008, Regents of the University of Colorado <br>
+ * All rights reserved.
  * <p>
  * 
  * A CollectionReader that loads all files in a directory tree.
@@ -60,73 +60,97 @@ import org.cleartk.util.io.Files;
 public class PlainTextCollectionReader extends CollectionReader_ImplBase {
 
 	/**
-	 * "FileOrDirectory" is a single, required, string parameter that takes 
-	 *  either the name of a single file or the root
-	 * directory containing all the files to be processed.
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_FILE_OR_DIRECTORY" is a
+	 * single, required, string parameter that takes either the name of a single
+	 * file or the root directory containing all the files to be processed.
 	 */
-	public static final String PARAM_FILE_OR_DIRECTORY = "FileOrDirectory";
+	public static final String PARAM_FILE_OR_DIRECTORY = "org.cleartk.util.PlainTextCollectionReader.PARAM_FILE_OR_DIRECTORY";
 
 	/**
-	 * "ViewName" is a single, optional, string parameter that takes the
-	 * the name that should be given to the JCas view associated with the document texts.
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_VIEW_NAME" is a single,
+	 * optional, string parameter that takes the the name that should be given
+	 * to the JCas view associated with the document texts.
+	 * 
 	 * @see UIMAUtil#createJCasView(CAS, String)
 	 */
-	public static final String PARAM_VIEW_NAME = "ViewName";
+	public static final String PARAM_VIEW_NAME = "org.cleartk.util.PlainTextCollectionReader.PARAM_VIEW_NAME";
 
 	/**
-	 * "Language" is a single, optional, string parameter that takes the
-	 * language code corresponding to the language of the documents being
-	 * examined. The value of this parameter is simply passed on to
-	 * JCas.setDocumentLanguage(String).
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_LANGUAGE" is a single,
+	 * optional, string parameter that takes the language code corresponding to
+	 * the language of the documents being examined. The value of this parameter
+	 * is simply passed on to JCas.setDocumentLanguage(String).
 	 * 
 	 * @see JCas#setDocumentLanguage(String)
 	 */
-	public static final String PARAM_LANGUAGE = "Language";
+	public static final String PARAM_LANGUAGE = "org.cleartk.util.PlainTextCollectionReader.PARAM_LANGUAGE";
 
 	/**
-	 * "Encoding" is a single, optional, string parameter that takes the
-	 * encoding of the text files (e.g. "UTF-8").
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_ENCODING" is a single,
+	 * optional, string parameter that takes the encoding of the text files
+	 * (e.g. "UTF-8").
 	 * 
 	 * @see Charset for a list of encoding names.
 	 */
-	public static final String PARAM_ENCODING = "Encoding";
+	public static final String PARAM_ENCODING = "org.cleartk.util.PlainTextCollectionReader.PARAM_ENCODING";
 
 	/**
-	 * "Suffixes" is a multiple, optional, string parameter that takes suffixes
-	 * (e.g. .txt) of the files that should be read in.  This parameter can only be set if there is no value for "FileNames".
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_SUFFIXES" is a
+	 * multiple, optional, string parameter that takes suffixes (e.g. .txt) of
+	 * the files that should be read in. This parameter can only be set if there
+	 * is no value for PARAM_FILE_NAMES_FILES or PARAM_FILE_NAMES.
 	 */
-	public static final String PARAM_SUFFIXES = "Suffixes";
+	public static final String PARAM_SUFFIXES = "org.cleartk.util.PlainTextCollectionReader.PARAM_SUFFIXES";
 
 	/**
-	 * "FileNames" is a multiple, optional, string parameter that names files
-	 * which contain lists of file names. For example, if the value
-	 * "mydata/mylist.txt" is provided, then the file "mylist.txt" should
-	 * contain a line delimited list of file names. The file names in the list
-	 * should not have directory information but should just be the names of the
-	 * files. The directory is determined by "FileOrDirectory" parameter and the
-	 * files that are processed result from traversing the directory structure
-	 * provided and looking for files with a name found in the lists of file
-	 * names. This parameter can only be set if there is no value for "Suffixes".
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_FILE_NAMES_FILES" is a
+	 * multiple, optional, string parameter that names files which contain lists
+	 * of file names. For example, if the value "mydata/mylist.txt" is provided,
+	 * then the file "mylist.txt" should contain a line delimited list of file
+	 * names. The file names in the list should not have directory information
+	 * but should just be the names of the files. The directory is determined by
+	 * PARAM_FILE_OR_DIRECTORY and the files that are processed result from
+	 * traversing the directory structure provided and looking for files with a
+	 * name found in the lists of file names. That is, no exception will be
+	 * thrown if a file name in the list does not actually correspond to a file.
+	 * This parameter can only be set if there is no value for PARAM_SUFFIXES or
+	 * PARAM_FILE_NAMES.
 	 */
-	public static final String PARAM_FILE_NAMES = "FileNames";
+	public static final String PARAM_FILE_NAMES_FILES = "org.cleartk.util.PlainTextCollectionReader.PARAM_FILE_NAMES_FILES";
+
+	/**
+	 * "org.cleartk.util.PlainTextCollectionReader.PARAM_FILE_NAMES" is a
+	 * multiple, optional, string parameter that provides a list of file names
+	 * that should be read in. The directory of the file names is determined by
+	 * PARAM_FILE_OR_DIRECTORY and the files that are processed result from
+	 * traversing the directory structure provided and looking for files with a
+	 * name found in the list of file names. That is, no exception will be
+	 * thrown if a file name in the list does not actually correspond to a file.
+	 * This parameter can only be set if there is no value for PARAM_SUFFIXES or
+	 * PARAM_FILE_NAMES_FILES.
+	 */
+	public static final String PARAM_FILE_NAMES = "org.cleartk.util.PlainTextCollectionReader.PARAM_FILE_NAMES";
 
 	@Override
 	public void initialize() throws ResourceInitializationException {
 		super.initialize();
 
 		// get the name of the CAS view to be added
-		this.viewName = (String) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PlainTextCollectionReader.PARAM_VIEW_NAME, null);
+		this.viewName = (String) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(),
+				PlainTextCollectionReader.PARAM_VIEW_NAME, null);
 
 		// get the language
-		this.language = (String) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PlainTextCollectionReader.PARAM_LANGUAGE, null);
+		this.language = (String) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(),
+				PlainTextCollectionReader.PARAM_LANGUAGE, null);
 
 		// get the encoding
-		this.encoding = (String) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PlainTextCollectionReader.PARAM_ENCODING, null);
+		this.encoding = (String) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(),
+				PlainTextCollectionReader.PARAM_ENCODING, null);
 
 		// get the input directory
 		String fileName = (String) UIMAUtil.getRequiredConfigParameterValue(this.getUimaContext(),
 				PlainTextCollectionReader.PARAM_FILE_OR_DIRECTORY);
-		
+
 		this.rootFile = new File(fileName);
 
 		// raise an exception if the root file does not exist
@@ -136,34 +160,45 @@ public class PlainTextCollectionReader extends CollectionReader_ImplBase {
 			throw new ResourceInitializationException(new IOException(message));
 		}
 
-		String[] suffixNames = (String[]) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PARAM_SUFFIXES, null);
-		String[] fileNamesLists = (String[]) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PARAM_FILE_NAMES, null);
+		String[] suffixNames = (String[]) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PARAM_SUFFIXES,
+				null);
+		String[] fileNamesLists = (String[]) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(),
+				PARAM_FILE_NAMES_FILES, null);
+		String[] fileNames = (String[]) UIMAUtil.getDefaultingConfigParameterValue(getUimaContext(), PARAM_FILE_NAMES,
+				null);
 
-		if (suffixNames != null && fileNamesLists != null) {
-				String message = String.format("One of the parameters %1$s or %2$s may be set but not both.", PARAM_SUFFIXES, PARAM_FILE_NAMES);
-				throw new ResourceInitializationException(new IllegalArgumentException(message));
+		if (!(suffixNames != null ^ fileNamesLists != null ^ fileNames != null)
+				&& (suffixNames != null || fileNamesLists != null || fileNames != null)) {
+			String message = String.format(
+					"One of the parameters %1$s, %2$s, or %3$s may be set but not more than one of them.",
+					PARAM_SUFFIXES, PARAM_FILE_NAMES_FILES, PARAM_FILE_NAMES);
+			throw new ResourceInitializationException(new IllegalArgumentException(message));
 		}
 
-		if(suffixNames != null) {
+		if (suffixNames != null) {
 			files = Files.getFiles(rootFile, suffixNames).iterator();
 			filesCount = countFiles(Files.getFiles(rootFile, suffixNames).iterator());
-		} else if (fileNamesLists != null) {
-			Set<String> fileNames = new HashSet<String>();
+		}
+		else if (fileNamesLists != null) {
+			Set<String> fileNamesFromLists = new HashSet<String>();
 			try {
-				for(String fileNamesList : fileNamesLists) {
-					fileNames.addAll(Arrays.asList(FileUtil.loadListOfStrings(new File(fileNamesList))));
+				for (String fileNamesList : fileNamesLists) {
+					fileNamesFromLists.addAll(Arrays.asList(FileUtil.loadListOfStrings(new File(fileNamesList))));
 				}
-				files = Files.getFiles(rootFile, fileNames).iterator();
-				filesCount = countFiles(Files.getFiles(rootFile, fileNames).iterator());
-			} catch(IOException ioe) {
+				files = Files.getFiles(rootFile, fileNamesFromLists).iterator();
+				filesCount = countFiles(Files.getFiles(rootFile, fileNamesFromLists).iterator());
+			}
+			catch (IOException ioe) {
 				throw new ResourceInitializationException(ioe);
 			}
+		}
+		else if (fileNames != null) {
+			files = Files.getFiles(rootFile, new HashSet<String>(Arrays.asList(fileNames))).iterator();
 		}
 		else {
 			files = Files.getFiles(rootFile).iterator();
 			filesCount = countFiles(Files.getFiles(rootFile).iterator());
 		}
-		
 
 	}
 
@@ -186,19 +221,20 @@ public class PlainTextCollectionReader extends CollectionReader_ImplBase {
 		String path;
 		if (this.rootFile.isDirectory()) {
 			path = Files.stripRootDir(rootFile, file);
-		} else {
+		}
+		else {
 			path = file.getPath();
 		}
-		
+
 		// set the document URI
 		ViewURIUtil.setURI(cas, path);
-		
+
 		completed++;
 	}
 
 	protected int countFiles(Iterator<File> tempFiles) {
 		int count = 0;
-		while(tempFiles.hasNext()) {
+		while (tempFiles.hasNext()) {
 			tempFiles.next();
 			count++;
 		}
@@ -224,10 +260,10 @@ public class PlainTextCollectionReader extends CollectionReader_ImplBase {
 
 	protected Iterator<File> files;
 
-	protected int completed = 0; 
-	
-	protected int filesCount = 0;
-	
-	public void close() throws IOException { }
-}
+	protected int completed = 0;
 
+	protected int filesCount = 0;
+
+	public void close() throws IOException {
+	}
+}
