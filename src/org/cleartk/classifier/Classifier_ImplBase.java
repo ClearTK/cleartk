27d9c72415c963a5272282d2a830151f606190ba
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-14 20:45:46 +0000

    stripped out sequential code
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@468 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- src/org/cleartk/classifier/Classifier_ImplBase.java
+++ src/org/cleartk/classifier/Classifier_ImplBase.java
@@ -26,7 +26,6 @@ package org.cleartk.classifier;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarFile;
@@ -35,7 +34,6 @@ import java.util.zip.ZipEntry;
 import org.cleartk.classifier.encoder.features.FeaturesEncoder;
 import org.cleartk.classifier.encoder.features.FeaturesEncoder_ImplBase;
 import org.cleartk.classifier.encoder.outcome.OutcomeEncoder;
-import org.cleartk.classifier.feature.extractor.outcome.OutcomeFeatureExtractor;
 import org.cleartk.util.ReflectionUtil;
 
 
@@ -47,16 +45,10 @@ import org.cleartk.util.ReflectionUtil;
 
 public abstract class Classifier_ImplBase<INPUTOUTCOME_TYPE,OUTPUTOUTCOME_TYPE,FEATURES_TYPE> implements Classifier<INPUTOUTCOME_TYPE> {
 	
-//	protected ClassifierManifest classifierManifest;
 	protected FeaturesEncoder<FEATURES_TYPE> featuresEncoder;
 	protected OutcomeEncoder<INPUTOUTCOME_TYPE,OUTPUTOUTCOME_TYPE> outcomeEncoder;
-	protected OutcomeFeatureExtractor[] outcomeFeatureExtractors;
-
 	
 	public Classifier_ImplBase(JarFile modelFile) throws IOException {
-		
-		// read the manifest
-//		classifierManifest = new ClassifierManifest(modelFile);
 		
 		// de-serialize the encoders
 		ZipEntry zipEntry = modelFile.getEntry(FeaturesEncoder_ImplBase.ENCODER_FILE_NAME);
@@ -75,35 +67,6 @@ public abstract class Classifier_ImplBase<INPUTOUTCOME_TYPE,OUTPUTOUTCOME_TYPE,F
 		this.featuresEncoder.allowNewFeatures(false);
 		this.outcomeEncoder = outcomeEncoderCast(genericOutcomeEncoder);
 		
-		zipEntry = modelFile.getEntry(DataWriter_ImplBase.OUTCOME_FEATURE_EXTRACTOR_FILE_NAME);
-		if(zipEntry == null) {
-			outcomeFeatureExtractors = new OutcomeFeatureExtractor[0];
-		} else {
-			is = new ObjectInputStream(modelFile.getInputStream(zipEntry));
-		
-			try {
-				outcomeFeatureExtractors = (OutcomeFeatureExtractor[]) is.readObject();
-			}
-			catch (ClassNotFoundException e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
-
-	public abstract INPUTOUTCOME_TYPE classify(List<Feature> features);
-
-	public List<INPUTOUTCOME_TYPE> classifySequence(List<List<Feature>> features) {
-		List<Object> outcomes = new ArrayList<Object>();
-		List<INPUTOUTCOME_TYPE> returnValues = new ArrayList<INPUTOUTCOME_TYPE>();
-		for (List<Feature> instanceFeatures : features) {
-			for(OutcomeFeatureExtractor outcomeFeatureExtractor : outcomeFeatureExtractors) {
-				instanceFeatures.addAll(outcomeFeatureExtractor.extractFeatures(outcomes));
-			}
-			INPUTOUTCOME_TYPE outcome = classify(instanceFeatures);
-			outcomes.add(outcome);
-			returnValues.add(outcome);
-		}
-		return returnValues;
 	}
 
 	public List<ScoredOutcome<INPUTOUTCOME_TYPE>> score(List<Feature> features, int maxResults) {
@@ -111,8 +74,6 @@ public abstract class Classifier_ImplBase<INPUTOUTCOME_TYPE,OUTPUTOUTCOME_TYPE,F
 	}
 
 
-	public abstract boolean isSequential();
-	
 	protected Class<?> getMyTypeArgument(String parameterName) {
 		return getTypeArgument(Classifier_ImplBase.class, parameterName, this);
 	}
