3e0b58cda0d56597bffbdbfcf2e9f171628dd649
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-14 15:06:58 +0000

    added getJCas() and getTypeSystemDescription() methods that provide default instances of these.
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@435 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- test/src/org/cleartk/util/TestsUtil.java
+++ test/src/org/cleartk/util/TestsUtil.java
@@ -34,11 +34,15 @@ import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.cleartk.classifier.AnnotationHandler;
 import org.cleartk.classifier.Feature;
 import org.cleartk.classifier.Instance;
 import org.cleartk.classifier.InstanceConsumer;
 import org.cleartk.syntax.treebank.type.TreebankNode;
+import org.uutuc.factory.AnalysisEngineFactory;
+import org.uutuc.factory.TypeSystemDescriptionFactory;
+import org.uutuc.util.JCasAnnotatorAdapter;
 
 /**
  * <br>
@@ -52,7 +56,38 @@ import org.cleartk.syntax.treebank.type.TreebankNode;
  */
 public class TestsUtil {
 
+	public static ThreadLocal<JCas> JCAS = new ThreadLocal<JCas>();
+	public static ThreadLocal<TypeSystemDescription> TYPE_SYSTEM = new ThreadLocal<TypeSystemDescription>();
+	public static ThreadLocal<AnalysisEngine> AE_ADAPTER = new ThreadLocal<AnalysisEngine>();
 
+	static {
+		try {
+			TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription("org.cleartk.TypeSystem");
+			TYPE_SYSTEM.set(tsd);
+
+			AnalysisEngine aeAdapter = AnalysisEngineFactory.createAnalysisEngine(
+					JCasAnnotatorAdapter.class,
+					tsd);
+			AE_ADAPTER.set(aeAdapter);
+			
+			JCas jCas = aeAdapter.newJCas();
+			JCAS.set(jCas);
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+	}
+	
+	public static JCas getJCas() {
+		JCas jCas = JCAS.get();
+		jCas.reset();
+		return jCas;
+	}
+
+	public static TypeSystemDescription getTypeSystemDescription() {
+		return TYPE_SYSTEM.get();
+	}
 
 	/**
 	 * Initializes the instance producer with the AnalysisEngine's context, and
