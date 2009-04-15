6e445093d7494af36cd1edf7a34131b64948938b
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-15 21:49:23 +0000

    added 3 tests: - tests descriptor file param declarations (only looks for correct param name) - tests descriptor file param settings (only looks for correct param name) - tests java source for comment for parameter (checks to see if param value is given)
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@531 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- test/src/org/cleartk/ParameterNamesTest.java
+++ test/src/org/cleartk/ParameterNamesTest.java
@@ -24,11 +24,18 @@
 package org.cleartk;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.uima.util.FileUtils;
 import org.cleartk.util.io.Files;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,32 +48,32 @@ import org.junit.Test;
  */
 public class ParameterNamesTest {
 
-	
 	@Test
-	public void testParameters() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException {
+	public void testParameterDefinitions() throws ClassNotFoundException, IllegalArgumentException,
+			IllegalAccessException {
 		List<String> badParameters = new ArrayList<String>();
 		Iterable<File> files = Files.getFiles("src", new String[] { ".java" });
-		
+
 		for (File file : files) {
 			String className = file.getPath();
 			className = className.substring(4);
-			className = className.substring(0, className.length()-5);
+			className = className.substring(0, className.length() - 5);
 			className = className.replace(File.separatorChar, '.');
-			
+
 			Class<?> cls = Class.forName(className);
 			Field[] fields = cls.getDeclaredFields();
-			for(Field field : fields) {
+			for (Field field : fields) {
 				String fieldName = field.getName();
-				if(fieldName.indexOf("PARAM") != -1) {
-					String expectedValue = className+"."+field.getName();
-					String actualValue = (String) field.get(cls); 
-					if(!expectedValue.equals(actualValue)) {
-						badParameters.add("'"+actualValue+"' should be '"+expectedValue+"'");
+				if (fieldName.indexOf("PARAM") != -1) {
+					String expectedValue = className + "." + field.getName();
+					String actualValue = (String) field.get(cls);
+					if (!expectedValue.equals(actualValue)) {
+						badParameters.add("'" + actualValue + "' should be '" + expectedValue + "'");
 					}
 				}
 			}
 		}
-		
+
 		if (badParameters.size() > 0) {
 			String message = String.format("%d descriptor parameters with bad names. ", badParameters.size());
 			System.err.println(message);
@@ -76,5 +83,140 @@ public class ParameterNamesTest {
 			Assert.fail(message);
 		}
 
+	}
+
+	@Test
+	public void testParameterComments() throws ClassNotFoundException, IllegalArgumentException,
+			IllegalAccessException, IOException {
+		List<String> badComments = new ArrayList<String>();
+		Iterable<File> files = Files.getFiles("src", new String[] { ".java" });
+
+		for (File file : files) {
+			String fileContents = FileUtils.file2String(file);
+			String className = file.getPath();
+			className = className.substring(4);
+			className = className.substring(0, className.length() - 5);
+			className = className.replace(File.separatorChar, '.');
+
+			Class<?> cls = Class.forName(className);
+			Field[] fields = cls.getDeclaredFields();
+			for (Field field : fields) {
+				String fieldName = field.getName();
+				if (fieldName.indexOf("PARAM") != -1) {
+					String actualValue = "\""+(String) field.get(cls)+"\"";
+					int index = fileContents.indexOf(actualValue);
+					if(index == -1) {
+						throw new RuntimeException("field name definition is not found by searching the contents of .java file.  field = "+actualValue);
+					}
+					index = fileContents.indexOf(actualValue, index+actualValue.length());
+					if(index == -1) {
+						badComments.add(actualValue+" in "+file.getPath()+" is not documented adequately.   The value of the param name should be provided along with the type (String, Boolean, Integer, Float), whether it is multi-valued, and whether it is mandatory along with a description of the parameter.");
+					}
+				}
+			}
+		}
+
+		if (badComments.size() > 0) {
+			String message = String.format("%d descriptor parameters with insufficient comments. ", badComments.size());
+			System.err.println(message);
+			for (String badParameter : badComments) {
+				System.err.println(badParameter);
+			}
+			Assert.fail(message);
+		}
+
+	}
+
+	
+	@Test
+	public void testDescriptorParameters() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException,
+			JDOMException, IOException {
+		List<String> badParameters = new ArrayList<String>();
+		Iterable<File> files = Files.getFiles("src", new String[] { ".xml" });
+
+		Namespace ns = Namespace.getNamespace("http://uima.apache.org/resourceSpecifier");
+		for (File file : files) {
+			SAXBuilder builder = new SAXBuilder();
+			Document doc = builder.build(file);
+			Element root = doc.getRootElement();
+			Element analysisEngineMetaData = root.getChild("analysisEngineMetaData", ns);
+			if(analysisEngineMetaData == null) {
+				continue;
+			}
+			Element configurationParametersParent = analysisEngineMetaData.getChild("configurationParameters", ns);
+			if (configurationParametersParent != null) {
+				List<?> configurationParameters = configurationParametersParent.getChildren("configurationParameter", ns);
+				for (Object configurationParameter : configurationParameters) {
+					String parameterName = ((Element) configurationParameter).getChildText("name", ns);
+					if (!validateParameterName(parameterName)) {
+						badParameters.add("bad parameter name '" + parameterName + "' in " + file.getPath());
+					}
+				}
+			}
+		}
+
+		if (badParameters.size() > 0) {
+			String message = String.format("%d descriptor parameters with bad names. ", badParameters.size());
+			System.err.println(message);
+			for (String badParameter : badParameters) {
+				System.err.println(badParameter);
+			}
+			Assert.fail(message);
+		}
+
+	}
+
+	@Test
+	public void testDescriptorParameterSettings() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException,
+			JDOMException, IOException {
+		List<String> badParameters = new ArrayList<String>();
+		Iterable<File> files = Files.getFiles("src", new String[] { ".xml" });
+
+		Namespace ns = Namespace.getNamespace("http://uima.apache.org/resourceSpecifier");
+		for (File file : files) {
+			SAXBuilder builder = new SAXBuilder();
+			Document doc = builder.build(file);
+			Element root = doc.getRootElement();
+			Element analysisEngineMetaData = root.getChild("analysisEngineMetaData", ns);
+			if(analysisEngineMetaData == null) {
+				continue;
+			}
+			Element configurationParametersParent = analysisEngineMetaData.getChild("configurationParameterSettings", ns);
+			if (configurationParametersParent != null) {
+				List<?> configurationParameters = configurationParametersParent.getChildren("nameValuePair", ns);
+				for (Object configurationParameter : configurationParameters) {
+					String parameterName = ((Element) configurationParameter).getChildText("name", ns);
+					if (!validateParameterName(parameterName)) {
+						badParameters.add("bad parameter setting '" + parameterName + "' in " + file.getPath());
+					}
+				}
+			}
+		}
+
+		if (badParameters.size() > 0) {
+			String message = String.format("%d descriptor parameter settings with bad names. ", badParameters.size());
+			System.err.println(message);
+			for (String badParameter : badParameters) {
+				System.err.println(badParameter);
+			}
+			Assert.fail(message);
+		}
+
+	}
+
+	private boolean validateParameterName(String parameterName) {
+		if (parameterName.indexOf('.') == -1) {
+			return false;
+		}
+		try {
+			String className = parameterName.substring(0, parameterName.lastIndexOf('.'));
+			String fieldName = parameterName.substring(parameterName.lastIndexOf('.') + 1);
+			Class<?> cls = Class.forName(className);
+			cls.getField(fieldName);
+		}
+		catch (Exception e) {
+			return false;
+		}
+		return true;
 	}
 }
