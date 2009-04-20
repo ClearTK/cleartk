816ba67a675f25c6addacff1942593065cb4fa11
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-20 16:53:05 +0000

    initial commit has compiler warnings
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@550 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ src/org/cleartk/classifier/opennlp/DefaultMaxentDataWriterFactory.java
@@ -0,0 +1,32 @@
+package org.cleartk.classifier.opennlp;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.cleartk.classifier.DataWriter;
+import org.cleartk.classifier.DataWriterFactory_ImplBase;
+import org.cleartk.classifier.encoder.factory.NameNumberEncoderFactory;
+import org.cleartk.classifier.encoder.features.FeaturesEncoder;
+import org.cleartk.classifier.encoder.features.NameNumber;
+import org.cleartk.classifier.encoder.outcome.OutcomeEncoder;
+
+public class DefaultMaxentDataWriterFactory extends DataWriterFactory_ImplBase {
+
+	@Override
+	public void initialize(UimaContext context) {
+		NameNumberEncoderFactory nnef = new NameNumberEncoderFactory();
+		featuresEncoder = nnef.createFeaturesEncoder(context);
+		outcomeEncoder = nnef.createOutcomeEncoder(context);
+	}
+	
+	@Override
+	public DataWriter<?> createDataWriter(File outputDirectory) throws IOException {
+		MaxentDataWriter mdw = new MaxentDataWriter(outputDirectory);
+		mdw.setFeaturesEncoder((FeaturesEncoder<List<NameNumber>>)getFeaturesEncoder());
+		mdw.setOutcomeEncoder((OutcomeEncoder<String, String>)getOutcomeEncoder());
+		return mdw;
+	}
+
+}
