b28998ef49ae6910462b657ba9d8b349f11caea3
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-20 17:02:25 +0000

    moved into own package called 'pos'
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@554 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- src/org/cleartk/example/pos/ExamplePOSAnnotationHandler.java
+++ src/org/cleartk/example/pos/ExamplePOSAnnotationHandler.java
@@ -21,7 +21,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE. 
 */
-package org.cleartk.example;
+package org.cleartk.example.pos;
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- src/org/cleartk/example/pos/ExamplePOSPlainTextWriter.java
+++ src/org/cleartk/example/pos/ExamplePOSPlainTextWriter.java
@@ -21,7 +21,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE. 
 */
-package org.cleartk.example;
+package org.cleartk.example.pos;
 
 import java.io.File;
 import java.io.FileNotFoundException;
