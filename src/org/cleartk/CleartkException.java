d53bf647b0cbba940dce3279186226fa118e7392
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-04-23 20:47:34 +0000

    added constructors from super class
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@594 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- src/org/cleartk/CleartkException.java
+++ src/org/cleartk/CleartkException.java
@@ -2,4 +2,20 @@ package org.cleartk;
 
 public class CleartkException extends Exception{
 
+	public CleartkException() {
+		super();
+	}
+
+	public CleartkException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CleartkException(String message) {
+		super(message);
+	}
+
+	public CleartkException(Throwable cause) {
+		super(cause);
+	}
+
 }
