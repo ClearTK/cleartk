01568ae9b67a3211794572e8bcabb43e7041e9db
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-07-31 00:02:04 +0000

    added line featuresEncoder.allowNewFeatures(true); this prevents empty instances from being passed to the classifier.
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@849 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- src/org/cleartk/classifier/mallet/MalletCRFClassifier.java
+++ src/org/cleartk/classifier/mallet/MalletCRFClassifier.java
@@ -63,6 +63,7 @@ public class MalletCRFClassifier extends SequentialClassifier_ImplBase<String,St
 		ZipEntry modelEntry = modelFile.getEntry("model.malletcrf");
 		ObjectInputStream objectStream = new ObjectInputStream(modelFile.getInputStream(modelEntry));
 		this.transducer = (Transducer) objectStream.readObject();
+		this.featuresEncoder.allowNewFeatures(true);
     }
 	
 	/**
