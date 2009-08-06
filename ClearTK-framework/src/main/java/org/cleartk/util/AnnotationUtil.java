b5b9d5019d5b9b5eae3bc34ae9022d6fd1cf5c14
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-08-06 00:01:56 +0000

    moved methods to AnnotationUtil2
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@928 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- ClearTK-framework/src/main/java/org/cleartk/util/AnnotationUtil.java
+++ ClearTK-framework/src/main/java/org/cleartk/util/AnnotationUtil.java
@@ -27,13 +27,9 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import opennlp.tools.util.Span;
-
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.tcas.Annotation;
-import org.cleartk.type.ContiguousAnnotation;
-import org.cleartk.type.SplitAnnotation;
 
 
 /**
@@ -46,27 +42,6 @@ import org.cleartk.type.SplitAnnotation;
  * 
  */
 public class AnnotationUtil {
-	/**
-	 * Determines whether the big annotation contains the small annotation
-	 * w.r.t. to character offsets (begin and end).
-	 * 
-	 */
-	public static boolean contains(Annotation bigAnnotation, Annotation smallAnnotation) {
-		if (bigAnnotation instanceof SplitAnnotation) {
-			FSArray splits = ((SplitAnnotation) bigAnnotation).getAnnotations();
-
-			for (int i = 0; i < splits.size(); i++) {
-				ContiguousAnnotation split = (ContiguousAnnotation) splits.get(i);
-				if (contains(split, smallAnnotation)) return true;
-			}
-			return false;
-		}
-
-		if (bigAnnotation == null || smallAnnotation == null) return false;
-		if (bigAnnotation.getBegin() <= smallAnnotation.getBegin()
-				&& bigAnnotation.getEnd() >= smallAnnotation.getEnd()) return true;
-		else return false;
-	}
 
 	public static boolean overlaps(Annotation annotation1, Annotation annotation2) {
 		Annotation firstAnnotation, secondAnnotation;
@@ -94,18 +69,6 @@ public class AnnotationUtil {
 		catch (Exception e) {
 			return 0;
 		}
-	}
-
-	public static Span getAnnotationsExtent(List<? extends Annotation> annotations) {
-		int start = Integer.MAX_VALUE;
-		int end = 0;
-
-		for (Annotation annotation : annotations) {
-			if (annotation.getBegin() < start) start = annotation.getBegin();
-			if (annotation.getEnd() > end) end = annotation.getEnd();
-		}
-
-		return new Span(start, end);
 	}
 
 	/**
