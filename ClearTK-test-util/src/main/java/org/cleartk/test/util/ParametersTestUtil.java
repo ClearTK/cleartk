06aea8640531e1384074c7bdcbbfd1593dda35c4
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2009-08-17 18:57:51 +0000

    initial commit of method that helps test for correctly described configuration parameters.
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@1073 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ ClearTK-test-util/src/main/java/org/cleartk/test/util/ParametersTestUtil.java
@@ -0,0 +1,108 @@
+/** 
+ * Copyright (c) 2009, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
+package org.cleartk.test.util;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.uutuc.factory.ConfigurationParameterFactory;
+
+/**
+ * <br>
+ * Copyright (c) 2009, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * 
+ * @author Philip Ogren
+ */
+public class ParametersTestUtil {
+
+	public static void testParameterDefinitions(Iterable<File> files) throws ClassNotFoundException {
+		List<String> badParameters = new ArrayList<String>();
+		List<String> missingParameterNameFields = new ArrayList<String>();
+		
+		for (File file : files) {
+			String className = file.getPath();
+			className = className.substring(14);
+			className = className.substring(0, className.length() - 5);
+			className = className.replace(File.separatorChar, '.');
+			Class<?> cls = Class.forName(className);
+			Field[] fields = cls.getDeclaredFields();
+			for (Field field : fields) {
+				if(ConfigurationParameterFactory.isConfigurationParameterField(field)) {
+					org.uutuc.descriptor.ConfigurationParameter annotation = field.getAnnotation(org.uutuc.descriptor.ConfigurationParameter.class);
+					String parameterName = annotation.name();
+					String expectedName = className + "." + field.getName();
+					if (!expectedName.equals(parameterName)) {
+						badParameters.add("'" + parameterName + "' should be '" + expectedName+ "'");
+					}
+					String fieldName = getParameterNameField(expectedName);
+					try {
+						Field fld = cls.getDeclaredField(fieldName);
+						if((fld.getModifiers() & Modifier.PUBLIC) == 0 ||
+							(fld.getModifiers() & Modifier.FINAL) == 0 ||
+							(fld.getModifiers() & Modifier.PUBLIC) == 0) {
+							missingParameterNameFields.add(expectedName);
+						}
+						else if(!fld.get(null).equals(expectedName)) {
+							missingParameterNameFields.add(expectedName);
+						}
+					} catch(Exception e) {
+						missingParameterNameFields.add(expectedName);
+					}
+				}
+			}
+		}
+
+		if (badParameters.size() > 0 || missingParameterNameFields.size() > 0) {
+			String message = String.format("%d descriptor parameters with bad names and %d descriptor parameters with no name field. ", badParameters.size(), missingParameterNameFields.size());
+			System.err.println(message);
+			System.err.println("descriptor parameters with bad names: ");
+			for (String badParameter : badParameters) {
+				System.err.println(badParameter);
+			}
+			System.err.println("each configuration parameter should have a public static final String that specifies its name.  The missing fields are: ");
+			for (String missingParameterNameField : missingParameterNameFields) {
+				System.err.println(missingParameterNameField +" should be named by "+ missingParameterNameField.substring(0, missingParameterNameField.lastIndexOf('.'))+"."+getParameterNameField(missingParameterNameField));
+			}
+			Assert.fail(message);
+		}
+	}
+	
+	
+	private static String getParameterNameField(String parameterName) {
+		String parameterNameField = "PARAM";
+
+		String fieldName = parameterName.substring(parameterName.lastIndexOf('.')+1);
+		String[] fieldNameParts = fieldName.split("(?=[A-Z]++)");
+		for(String fieldNamePart : fieldNameParts) {
+			parameterNameField += "_" + fieldNamePart.toUpperCase();
+		}
+		return parameterNameField;
+	}
+	
+}
