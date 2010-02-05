6bef04da6fa35c84275d7df3cb939599afcdc376
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2010-02-05 04:10:03 +0000

    fixed copyright statement
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@1615 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- ClearTK-toolkit/src/main/java/org/cleartk/srl/propbank/PropbankFormatWriter.java
+++ ClearTK-toolkit/src/main/java/org/cleartk/srl/propbank/PropbankFormatWriter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2007-2008, Regen ts of the University of Colorado 
+ * Copyright (c) 2010, Regents of the University of Colorado 
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
