22c304cbaafb4deb13fceb6a30256e5bcc39ddd3
phwetzler <phwetzler@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2010-04-23 01:59:35 +0000

    fixed import error in OpenNLPTaggerParser
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@1641 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- ClearTK-toolkit/src/main/java/org/cleartk/syntax/opennlp/OpenNLPTaggerParser.java
+++ ClearTK-toolkit/src/main/java/org/cleartk/syntax/opennlp/OpenNLPTaggerParser.java
@@ -29,9 +29,9 @@ import org.cleartk.syntax.treebank.type.TreebankNode;
 import org.cleartk.type.Sentence;
 import org.cleartk.type.Token;
 import org.cleartk.util.AnnotationRetrieval;
-import org.uutuc.descriptor.ConfigurationParameter;
-import org.uutuc.factory.ConfigurationParameterFactory;
-import org.uutuc.util.InitializeUtil;
+import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.factory.ConfigurationParameterFactory;
+import org.uimafit.util.InitializeUtil;
 
 public class OpenNLPTaggerParser extends JCasAnnotator_ImplBase {
 
