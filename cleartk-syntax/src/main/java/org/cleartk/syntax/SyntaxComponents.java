896dd5bd9adb20292c1449ba3af4de150f7e763c
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2010-10-19 22:47:24 +0000

    rearranged type system files in cleartk-syntax
    git-svn-id: https://cleartk.googlecode.com/svn/branches/CleartkProjectReOrg@1941 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- cleartk-syntax/src/main/java/org/cleartk/syntax/SyntaxComponents.java
+++ cleartk-syntax/src/main/java/org/cleartk/syntax/SyntaxComponents.java
@@ -6,5 +6,5 @@ import org.uimafit.factory.TypeSystemDescriptionFactory;
 public class SyntaxComponents {
 
 	public static TypeSystemDescription TYPE_SYSTEM_DESCRIPTION = TypeSystemDescriptionFactory
-	.createTypeSystemDescription("org.cleartk.syntax.constituent.type.TypeSystem");
+	.createTypeSystemDescription("org.cleartk.syntax.TypeSystem");
 }
