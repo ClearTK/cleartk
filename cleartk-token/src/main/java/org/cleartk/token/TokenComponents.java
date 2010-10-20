6d220c560baf89adc9bf21dd774fb9795042811a
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2010-10-20 03:36:04 +0000

    moved a bunch of code from cleartk-toolkit to cleartk-token
    git-svn-id: https://cleartk.googlecode.com/svn/branches/CleartkProjectReOrg@1953 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ cleartk-token/src/main/java/org/cleartk/token/TokenComponents.java
@@ -0,0 +1,10 @@
+package org.cleartk.token;
+
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.uimafit.factory.TypeSystemDescriptionFactory;
+
+public class TokenComponents {
+
+	public static TypeSystemDescription TYPE_SYSTEM_DESCRIPTION = TypeSystemDescriptionFactory
+	.createTypeSystemDescription("org.cleartk.token.TypeSystem");
+}
---
+++ cleartk-token/src/main/java/org/cleartk/token/stem/snowball/DefaultSnowballStemmer.java
@@ -0,0 +1,69 @@
+/** 
+ * Copyright (c) 2009, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+package org.cleartk.token.stem.snowball;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.cleartk.token.TokenComponents;
+import org.cleartk.token.type.Token;
+import org.uimafit.factory.AnalysisEngineFactory;
+
+/**
+ * <br>
+ * Copyright (c) 2009, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * 
+ * 
+ * @author Philip Ogren
+ * 
+ * 
+ *         This class borrows from
+ *         org.apache.lucene.analysis.snowball.SnowballFilter
+ * @see org.apache.lucene.analysis.snowball.SnowballFilter
+ */
+
+public class DefaultSnowballStemmer extends SnowballStemmer<Token> {
+	
+	public static AnalysisEngineDescription getDescription(String language)
+	throws ResourceInitializationException {
+		return AnalysisEngineFactory.createPrimitiveDescription(
+				DefaultSnowballStemmer.class, 
+				TokenComponents.TYPE_SYSTEM_DESCRIPTION,
+				SnowballStemmer.PARAM_STEMMER_NAME, language);
+	}
+
+	
+	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+	}
+
+	@Override
+	public void setStem(Token token, String stem) {
+		token.setStem(stem);
+	}
+
+}
