1651e3ca451e9a828796f332755abbffa3a04e87
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2010-12-27 20:50:58 +0000

    cleartk-reorg
    git-svn-id: https://cleartk.googlecode.com/svn/branches/CleartkProjectReOrg@2136 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- cleartk-ml/src/test/java/org/cleartk/classifier/ClassifierTestUtil.java
+++ cleartk-ml/src/test/java/org/cleartk/classifier/ClassifierTestUtil.java
@@ -21,7 +21,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE. 
  */
-package org.cleartk.util;
+package org.cleartk.classifier;
 
 import java.util.ArrayList;
 import java.util.List;
