d71e0ea0c584f90f855b8b09feac543c959acfaf
brian@wsnetwork.net <brian@wsnetwork.net@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2011-01-11 16:07:05 +0000

    Revert ClearTK-ml changes that were inadvertently committed to trunk.
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@2296 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- cleartk-ml/src/main/java/org/cleartk/classifier/util/featurevector/ArrayFeatureVector.java
+++ cleartk-ml/src/main/java/org/cleartk/classifier/util/featurevector/ArrayFeatureVector.java
@@ -1,13 +1,13 @@
-/**
- * Copyright (c) 2007-2008, Regents of the University of Colorado
+ /** 
+ * Copyright (c) 2007-2008, Regents of the University of Colorado 
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -19,8 +19,8 @@
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+ * POSSIBILITY OF SUCH DAMAGE. 
+*/
 package org.cleartk.classifier.util.featurevector;
 
 import java.util.ArrayList;
@@ -31,7 +31,7 @@ import org.cleartk.CleartkException;
 
 
 /**
- * <br>Copyright (c) 2007-2008, Regents of the University of Colorado
+ * <br>Copyright (c) 2007-2008, Regents of the University of Colorado 
  * <br>All rights reserved.
 
  *
@@ -40,93 +40,84 @@ import org.cleartk.CleartkException;
  */
 
 public class ArrayFeatureVector extends FeatureVector {
+	
+	ArrayList<Double> features;
+	
+	public ArrayFeatureVector() {
+		features = new ArrayList<Double>();
+	}
 
-    ArrayList<Double> features;
+	public double get(int index) {
+		if( index < features.size() )
+			return features.get(index);
+		else
+			return 0.0;
+	}
 
-    public ArrayFeatureVector() {
-        features = new ArrayList<Double>();
-    }
+	public java.util.Iterator<Entry> iterator() {
+		return new Iterator(features);
+	}
 
-    @Override
-    public double get(int index) {
-        if( index < features.size() )
-            return features.get(index);
-        else
-            return 0.0;
-    }
+	public void set(int index, double value) throws CleartkException {
+		if( Double.isInfinite(value) || Double.isNaN(value) )
+			throw new CleartkException(String.format("trying to set illegal value in %d:%.7f", index, value));
+		
+		for( int i=features.size(); i<=index; i++ )
+			features.add(0.0);
+		
+		features.set(index, value);
+	}
+	
+	public double[] toArray() {
+		double[] result = new double[features.size()];
+		int index = 0;
+		
+		for( Double d : features ) {
+			result[index] = d;
+			index += 1;
+		}
+			
+		return result;
+	}
 
-    @Override
-    public java.util.Iterator<Entry> iterator() {
-        return new Iterator(features);
-    }
+	static class Iterator implements java.util.Iterator<FeatureVector.Entry> {
+		
+		int currentIndex;
 
-    @Override
-    public java.util.Iterator<Entry> orderedIterator() {
-        // The values are always stored in order so the normal iterator can be used.
-        return iterator();
-    }
+		java.util.ListIterator<Double> subIterator;
+		
+		public Iterator(List<Double> features) {
+			currentIndex = 0;
+			subIterator = features.listIterator();
+			moveToNext();
+		}
+		
+		public boolean hasNext() {
+			return subIterator.hasNext();
+		}
 
-    @Override
-    public void set(int index, double value) throws CleartkException {
-        if( Double.isInfinite(value) || Double.isNaN(value) )
-            throw new CleartkException(String.format("trying to set illegal value in %d:%.7f", index, value));
+		public Entry next() {
+			int index = subIterator.nextIndex();
+			Double value = subIterator.next();
+			moveToNext();
+			
+			return new FeatureVector.Entry(index, value.doubleValue());
+		}
 
-        for( int i=features.size(); i<=index; i++ )
-            features.add(0.0);
-
-        features.set(index, value);
-    }
-
-    public double[] toArray() {
-        double[] result = new double[features.size()];
-        int index = 0;
-
-        for( Double d : features ) {
-            result[index] = d;
-            index += 1;
-        }
-
-        return result;
-    }
-
-    static class Iterator implements java.util.Iterator<FeatureVector.Entry> {
-
-        int currentIndex;
-
-        java.util.ListIterator<Double> subIterator;
-
-        public Iterator(List<Double> features) {
-            currentIndex = 0;
-            subIterator = features.listIterator();
-            moveToNext();
-        }
-
-        public boolean hasNext() {
-            return subIterator.hasNext();
-        }
-
-        public Entry next() {
-            int index = subIterator.nextIndex();
-            Double value = subIterator.next();
-            moveToNext();
-
-            return new FeatureVector.Entry(index, value.doubleValue());
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        private void moveToNext() {
-            while( subIterator.hasNext() ) {
-                Double d = subIterator.next();
-                if( d != null && d.doubleValue() != 0.0 ) {
-                    subIterator.previous();
-                    return;
-                }
-            }
-        }
-
-    }
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+		
+		private void moveToNext() {
+			while( subIterator.hasNext() ) {
+				Double d = subIterator.next();
+				if( d != null && d.doubleValue() != 0.0 ) {
+					subIterator.previous();
+					return;
+				}
+			}
+		}
+		
+	}
 
 }
