247314e6b0c994738eb23e442d2ec2c7af08279c
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2011-01-19 21:37:10 +0000

    initial commit of package-info.java which points a new user to the 20 newsgroups corpus
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@2330 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ cleartk-examples/src/main/java/org/cleartk/examples/documentclassification/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * The code in this package provides a simple example of document classification.  The example
+ * code assumes that the documents you want to classify are place in directories corresponding to
+ * their category.  A nice corpus that you can download with this property is the "20 newsgroups" corpus
+ * which can be found here (among other places):
+ * 
+ * http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.html
+ * 
+ */
+package org.cleartk.examples.documentclassification;
+
