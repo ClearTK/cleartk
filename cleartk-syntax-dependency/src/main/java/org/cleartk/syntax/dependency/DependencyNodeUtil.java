99ee975662ee4d1990d005422e32b79c6fadf19e
philip@ogren.info <philip@ogren.info@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2011-02-03 04:48:32 +0000

    removed junit assertions from code in src/main/java
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@2419 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- cleartk-syntax-dependency/src/main/java/org/cleartk/syntax/dependency/DependencyNodeUtil.java
+++ cleartk-syntax-dependency/src/main/java/org/cleartk/syntax/dependency/DependencyNodeUtil.java
@@ -24,15 +24,12 @@
 
 package org.cleartk.syntax.dependency;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
 import org.cleartk.syntax.dependency.type.DependencyNode;
 import org.cleartk.util.UIMAUtil;
-import org.junit.Test;
 
 /**
  * <br>
@@ -88,23 +85,5 @@ public class DependencyNodeUtil {
     return new String(indentChars);
   }
   
-  @Test
-  public void testGetNumberString() throws Exception {
-    assertEquals("", getNumberString(0));
-    assertEquals("0", getNumberString(1));
-    assertEquals("01", getNumberString(2));
-    assertEquals("012", getNumberString(3));
-    assertEquals("0123", getNumberString(4));
-    assertEquals("01234", getNumberString(5));
-    assertEquals("012345", getNumberString(6));
-    assertEquals("0123456", getNumberString(7));
-    assertEquals("01234567", getNumberString(8));
-    assertEquals("012345678", getNumberString(9));
-    assertEquals("0123456789", getNumberString(10));
-    assertEquals("01234567890", getNumberString(11));
-    assertEquals("012345678901", getNumberString(12));
-    assertEquals("0123456789012345678901234567890123456789", getNumberString(40));
-    assertEquals("01234567890123456789012345678901234567890123", getNumberString(44));
-    
-  }
+  
 }
