07f90d5639f8229c52f185c38ad2ef86199eaf21
dlonnon <dlonnon@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2011-05-10 02:57:20 +0000

    Adding Tree Kernel SVM Light adapter project.
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@2889 2f4ce1e4-9571-11dd-93b2-6d14b300b761

---
+++ cleartk-ml-tksvmlight/src/main/java/org/cleartk/classifier/tksvmlight/TKSVMlightDataWriter.java
@@ -0,0 +1,114 @@
+/** 
+ * Copyright (c) 2009, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
+package org.cleartk.classifier.tksvmlight;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import org.cleartk.classifier.CleartkProcessingException;
+import org.cleartk.classifier.encoder.CleartkEncoderException;
+import org.cleartk.classifier.jar.DataWriter_ImplBase;
+import org.cleartk.classifier.util.featurevector.FeatureVector;
+
+/**
+ * <br>
+ * Copyright (c) 2009, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * <p>
+ * 
+ * @author Daryl Lonnon
+ * @version 0.2.1
+ * 
+ * The data writer for the tree kernel svm light data writer.
+ */
+
+public class TKSVMlightDataWriter extends
+    DataWriter_ImplBase<TKSVMlightClassifierBuilder, TreeFeatureVector, Boolean, Boolean> {
+
+	/**
+	 * Constructor for the Tree Kernel SVM Light data writer.
+	 * @param outputDirectory The directory the data/files should be written within.
+	 * @throws IOException
+	 */
+  public TKSVMlightDataWriter(File outputDirectory) throws IOException {
+    super(outputDirectory);
+  }
+
+  /**
+   * creates and formats a string (without the outcome) for a specific feature vector. Public so that the one versus all data writer may use it, not for casual use.
+   * @param features The featuers to write into a string format.
+   * @return The string that represents the features in a format that tk_svm_classify can utilize.
+   * @throws CleartkProcessingException
+   */
+  public static String createString(TreeFeatureVector features) throws CleartkProcessingException
+  {
+    StringBuffer output = new StringBuffer();
+
+    for (String tree : features.getTrees().values())
+    {
+      output.append(String.format(Locale.US, " |BT| %s", tree));
+    }
+    if (!features.getTrees().isEmpty()) {
+      output.append(" |ET|");
+    }
+    
+    for (FeatureVector.Entry entry : features.getFeatures()) {
+      if (Double.isInfinite(entry.value) || Double.isNaN(entry.value))
+        throw CleartkEncoderException.invalidFeatureVectorValue(entry.index, entry.value);
+      output.append(String.format(Locale.US, " %d:%.7f", entry.index, entry.value));
+    }
+    
+    return output.toString();
+  }
+  
+  /**
+   * Write a line representing the feature vector with the appropriate outcome to be used by the tk_svm_classify command.
+   * @param features The feature vector to be written.
+   * @param outcome The correct classification for that feature vector.
+   */
+  @Override
+  public void writeEncoded(TreeFeatureVector features, Boolean outcome)
+      throws CleartkProcessingException {
+    StringBuffer output = new StringBuffer();
+
+    if (outcome == null) {
+      output.append("0");
+    } else if (outcome.booleanValue()) {
+      output.append("+1");
+    } else {
+      output.append("-1");
+    }
+    
+    output.append(createString(features));
+    
+    this.trainingDataWriter.println(output);
+  }
+
+  @Override
+  protected TKSVMlightClassifierBuilder newClassifierBuilder() {
+    return new TKSVMlightClassifierBuilder();
+  }
+
+}
---
+++ cleartk-ml-tksvmlight/src/test/java/org/cleartk/classifier/tksvmlight/RunTKSVMlightTest.java
@@ -0,0 +1,297 @@
+/** 
+ * Copyright (c) 2007-2008, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
+package org.cleartk.classifier.tksvmlight;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.uima.jcas.JCas;
+import org.cleartk.classifier.CleartkAnnotator;
+import org.cleartk.classifier.CleartkProcessingException;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.Instance;
+import org.cleartk.classifier.jar.DirectoryDataWriterFactory;
+import org.cleartk.classifier.jar.Train;
+import org.cleartk.test.DefaultTestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.uimafit.factory.UimaContextFactory;
+import org.uimafit.testing.util.HideOutput;
+
+/**
+ * <br>
+ * Copyright (c) 2007-2008, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * 
+ * @author Steven Bethard, Philipp Wetzler
+ */
+public class RunTKSVMlightTest extends DefaultTestBase {
+
+  protected String dataDirectory = "src/test/resources/data/svmlight/tk";
+
+  @Test
+  public void testPath() throws Exception {
+    String[] command = new String[] { "svm_learn" };
+
+    try {
+      Process process = Runtime.getRuntime().exec(command);
+      process.getOutputStream().write('\n');
+      process.getOutputStream().write('\n');
+      process.getOutputStream().close();
+      slurp(process.getInputStream());
+      slurp(process.getErrorStream());
+      process.waitFor();
+    } catch (IOException e) {
+      throw e;
+    }
+  }
+
+  // @Test
+  public void testTreeKernel() throws Exception {
+/*    File dir = new File(dataDirectory, "nonlinear");
+    File trainingFile = new File(dir, "training-data.svmlight");
+    File testFile = new File(dir, "test-data.svmlight");
+
+    trainAndTest(trainingFile, testFile, new String[] { "-t", "5" }, "tree kernel");
+    */
+  }
+/*
+  private void trainAndTest(File trainingFile, File testFile, String[] args, String name)
+      throws Exception {
+    File modelFile = new File(this.outputDirectoryName, "model.svmlight");
+
+    String[] command = new String[3 + args.length];
+    command[0] = "svm_learn";
+    for (int i = 0; i < args.length; i++)
+      command[i + 1] = args[i];
+    command[command.length - 2] = trainingFile.getPath();
+    command[command.length - 1] = modelFile.getPath();
+
+    Process process = Runtime.getRuntime().exec(command);
+    slurp(process.getInputStream());
+    output(process.getErrorStream(), System.err);
+    process.waitFor();
+
+    SVMlightModel model = SVMlightModel.fromFile(modelFile);
+    BufferedReader r = new BufferedReader(new FileReader(testFile));
+    float total = 0;
+    float correct = 0;
+    String line;
+    while ((line = r.readLine()) != null) {
+      String[] fields = line.split(" ");
+
+      boolean expectedResult = fields[0].equals("+1");
+
+      FeatureVector fv = new SparseFeatureVector();
+      for (int i = 1; i < fields.length; i++) {
+        String[] parts = fields[i].split(":");
+        int featureIndex = Integer.valueOf(parts[0]);
+        double featureValue = Double.valueOf(parts[1]);
+        fv.set(featureIndex, featureValue);
+      }
+
+      boolean actualResult = model.evaluate(fv) > 0;
+
+      total += 1;
+      if (expectedResult == actualResult)
+        correct += 1;
+    }
+    r.close();
+
+    if (correct < (total * 0.95))
+      Assert.fail("model accuracy using " + name + " is below 95%");
+  }
+
+  private static void output(InputStream input, PrintStream output) throws IOException {
+    byte[] buffer = new byte[128];
+    int count = input.read(buffer);
+    while (count != -1) {
+      output.write(buffer, 0, count);
+      count = input.read(buffer);
+    }
+  }
+*/
+  private static void slurp(InputStream input) throws IOException {
+    byte[] buffer = new byte[128];
+    int count = input.read();
+    while (count != -1) {
+      count = input.read(buffer);
+    }
+  }
+
+  private static class EmptyAnnotator<T> extends CleartkAnnotator<T> {
+    public EmptyAnnotator() {
+    }
+
+    @Override
+    public void process(JCas aJCas) {
+    }
+
+    public void write(Instance<T> instance) throws CleartkProcessingException {
+      this.dataWriter.write(instance);
+    }
+  }
+
+  @Test
+  public void testTKSVMlight() throws Exception {
+    // create the data writer
+    EmptyAnnotator<Boolean> annotator = new EmptyAnnotator<Boolean>();
+    annotator.initialize(UimaContextFactory.createUimaContext(
+        DirectoryDataWriterFactory.PARAM_OUTPUT_DIRECTORY,
+        this.outputDirectoryName,
+        CleartkAnnotator.PARAM_DATA_WRITER_FACTORY_CLASS_NAME,
+        DefaultTKSVMlightDataWriterFactory.class.getName()));
+
+    // add a bunch of instances
+    for (Instance<Boolean> instance : generateBooleanInstances(20)) {
+      annotator.write(instance);
+    }
+    annotator.collectionProcessComplete();
+
+    // check that the output file was written and is not empty
+    BufferedReader reader = new BufferedReader(new FileReader(new File(
+        this.outputDirectoryName,
+        "training-data.svmlight")));
+    Assert.assertTrue(reader.readLine().length() > 0);
+    reader.close();
+
+    // run the training command
+    HideOutput hider = new HideOutput();
+    Train.main(this.outputDirectoryName, "-t", "5", "-c", "1.0", "-C", "+");
+    hider.restoreOutput();
+
+    // read in the classifier and test it on new instances
+    TKSVMlightClassifierBuilder builder = new TKSVMlightClassifierBuilder();
+    TKSVMlightClassifier classifier;
+    classifier = builder.loadClassifierFromTrainingDirectory(this.outputDirectory);
+    for (Instance<Boolean> instance : generateBooleanInstances(20)) {
+      List<Feature> features = instance.getFeatures();
+      Boolean outcome = instance.getOutcome();
+      Assert.assertEquals(outcome, classifier.classify(features));
+    }
+  }
+
+  @Test
+  public void testOVATKSVMlight() throws Exception {
+    // create the data writer
+    EmptyAnnotator<String> annotator = new EmptyAnnotator<String>();
+    annotator.initialize(UimaContextFactory.createUimaContext(
+        DirectoryDataWriterFactory.PARAM_OUTPUT_DIRECTORY,
+        this.outputDirectoryName,
+        CleartkAnnotator.PARAM_DATA_WRITER_FACTORY_CLASS_NAME,
+        DefaultOVATKSVMlightDataWriterFactory.class.getName()));
+
+    // add a bunch of instances
+    for (Instance<String> instance : generateStringInstances(20)) {
+      annotator.write(instance);
+    }
+    annotator.collectionProcessComplete();
+
+    // check that the output files were written for each class
+    for (String fileName : new String[] {
+        "training-data-1.svmlight",
+        "training-data-2.svmlight",
+        "training-data-3.svmlight" }) {
+      BufferedReader reader = new BufferedReader(new FileReader(new File(
+          this.outputDirectoryName,
+          fileName)));
+      Assert.assertTrue(reader.readLine().length() > 0);
+      reader.close();
+    }
+
+    // run the training command
+    HideOutput hider = new HideOutput();
+    Train.main(this.outputDirectoryName, "-c", "0.01", "-t", "5", "-d", "2", "-C", "+");
+    hider.restoreOutput();
+
+    // read in the classifier and test it on new instances
+    OVATKSVMlightClassifierBuilder builder = new OVATKSVMlightClassifierBuilder();
+    OVATKSVMlightClassifier classifier;
+    classifier = builder.loadClassifierFromTrainingDirectory(this.outputDirectory);
+    for (Instance<String> instance : generateStringInstances(20)) {
+      List<Feature> features = instance.getFeatures();
+      String outcome = instance.getOutcome();
+      Assert.assertEquals(outcome, classifier.classify(features));
+    }
+ 
+  }
+
+  private static List<Instance<Boolean>> generateBooleanInstances(int n) {
+    Random random = new Random(42);
+    List<Instance<Boolean>> instances = new ArrayList<Instance<Boolean>>();
+    for (int i = 0; i < n; i++) {
+      Instance<Boolean> instance = new Instance<Boolean>();
+      if (random.nextInt(2) == 0) {
+        instance.setOutcome(true);
+        instance.add(new Feature("TK_tree", "(S (NP I) (VB ran) (. .))"));
+        instance.add(new Feature("hello", random.nextInt(100) + 1000));
+        instance.add(new Feature("goodbye", 500));
+      } else {
+        instance.setOutcome(false);
+        instance.add(new Feature("TK_tree", "(S (VB I) (NP ran) (. .))"));
+        instance.add(new Feature("hello", random.nextInt(100)));
+        instance.add(new Feature("goodbye", 500));
+      }
+      instances.add(instance);
+    }
+    return instances;
+  }
+
+  private static List<Instance<String>> generateStringInstances(int n) {
+    Random random = new Random(42);
+    List<Instance<String>> instances = new ArrayList<Instance<String>>();
+    for (int i = 0; i < n; i++) {
+      Instance<String> instance = new Instance<String>();
+      int c = random.nextInt(3);
+      if (c == 0) {
+        instance.setOutcome("A");
+        instance.add(new Feature("TK_tree", "(S (NP I) (VB ran) (. .))"));
+        instance.add(new Feature("hello", random.nextInt(100) + 950));
+        instance.add(new Feature("goodbye", random.nextInt(100)));
+        instance.add(new Feature("farewell", random.nextInt(100)));
+      } else if (c == 1) {
+        instance.setOutcome("B");
+        instance.add(new Feature("TK_tree", "(S (TT going) (ZZ gone) (. .))"));
+        instance.add(new Feature("hello", random.nextInt(100)));
+        instance.add(new Feature("goodbye", random.nextInt(100) + 950));
+        instance.add(new Feature("farewell", random.nextInt(100)));
+      } else {
+        instance.setOutcome("C");
+        instance.add(new Feature("TK_tree", "(S (DET The) (PP Fox) (. .))"));
+        instance.add(new Feature("hello", random.nextInt(100)));
+        instance.add(new Feature("goodbye", random.nextInt(100)));
+        instance.add(new Feature("farewell", random.nextInt(100) + 950));
+      }
+      instances.add(instance);
+    }
+    return instances;
+  }
+
+}
