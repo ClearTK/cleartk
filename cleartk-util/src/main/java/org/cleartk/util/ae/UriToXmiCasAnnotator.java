41823ef1cec7340262ccf0cf368a53c827baf811
lee.becker@gmail.com <lee.becker@gmail.com@2f4ce1e4-9571-11dd-93b2-6d14b300b761>
2012-08-02 03:40:46 +0000

    Added static convenience methods for getting descriptors of this AnalysisEngine.
    git-svn-id: https://cleartk.googlecode.com/svn/trunk@3954 2f4ce1e4-9571-11dd-93b2-6d14b300b761

--- cleartk-util/src/main/java/org/cleartk/util/ae/UriToXmiCasAnnotator.java
+++ cleartk-util/src/main/java/org/cleartk/util/ae/UriToXmiCasAnnotator.java
@@ -3,15 +3,21 @@ package org.cleartk.util.ae;
 import java.io.InputStream;
 import java.net.URI;
 
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.impl.XCASDeserializer;
 import org.apache.uima.cas.impl.XCASSerializer;
 import org.apache.uima.cas.impl.XmiCasDeserializer;
 import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.cleartk.util.ViewURIUtil;
 import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.component.ViewCreatorAnnotator;
 import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.factory.AggregateBuilder;
+import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.ConfigurationParameterFactory;
 
 /**
@@ -28,6 +34,28 @@ import org.uimafit.factory.ConfigurationParameterFactory;
  * 
  */
 public class UriToXmiCasAnnotator extends JCasAnnotator_ImplBase {
+
+  public static AnalysisEngineDescription getDescription() throws ResourceInitializationException {
+    return AnalysisEngineFactory.createPrimitiveDescription(UriToXmiCasAnnotator.class);
+  }
+
+  /**
+   * Use this aggregate description if UriToDocumentTextAnnotator will be writing to a mapped view.
+   * 
+   * @param targetViewName
+   * @return
+   * @throws ResourceInitializationException
+   */
+  public static AnalysisEngineDescription getCreateViewAggregateDescription(String targetViewName)
+      throws ResourceInitializationException {
+    AggregateBuilder builder = new AggregateBuilder();
+    builder.add(AnalysisEngineFactory.createPrimitiveDescription(
+        ViewCreatorAnnotator.class,
+        ViewCreatorAnnotator.PARAM_VIEW_NAME,
+        targetViewName));
+    builder.add(UriToXmiCasAnnotator.getDescription(), CAS.NAME_DEFAULT_SOFA, targetViewName);
+    return builder.createAggregateDescription();
+  }
 
   public static enum XmlScheme {
     XMI, XCAS
