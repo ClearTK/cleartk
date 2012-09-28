f86d0dae75942ee309f04425a4fa7113a501892f
Steven Bethard <steven.bethard@gmail.com>
2012-09-28 09:00:06 -0600

    Fixes the jcasgen-m2e plugin to also run if the output directory (e.g. target/generated-sources/jcasgen) is empty

--- jcasgen-m2e-connector/org.cleartk.plugin.jcasgen.m2e/src/org/cleartk/plugin/JCasGenBuildParticipant.java
+++ jcasgen-m2e-connector/org.cleartk.plugin.jcasgen.m2e/src/org/cleartk/plugin/JCasGenBuildParticipant.java
@@ -24,24 +24,33 @@ public class JCasGenBuildParticipant extends MojoExecutionBuildParticipant {
     IMaven maven = MavenPlugin.getMaven();
     BuildContext buildContext = getBuildContext();
 
-    // check if the type system file has changed
+    // get the Mojo parameters
     String typeSystem = maven.getMojoParameterValue(
         getSession(),
         getMojoExecution(),
         "typeSystem",
         String.class);
+    File generatedSourcesDirectory = maven.getMojoParameterValue(
+        getSession(),
+        getMojoExecution(),
+        "outputDirectory",
+        File.class);
 
-    // check for the type system file in the project base directory
-    for (MavenProject project : this.getSession().getProjects()) {
-      File typeSystemFile = new File(project.getBasedir(), typeSystem);
-      if (typeSystemFile.exists()) {
+    // only run the Mojo if the type system was updated (or the sources were not yet generated)
+    if (generatedSourcesDirectory != null && generatedSourcesDirectory.exists()) {
+      for (MavenProject project : this.getSession().getProjects()) {
+        File typeSystemFile = new File(project.getBasedir(), typeSystem);
+        if (typeSystemFile.exists()) {
 
-        // scanner only sees files with content changes since the last build
-        Scanner scanner = buildContext.newScanner(typeSystemFile);
-        scanner.scan();
-        String[] includedFiles = scanner.getIncludedFiles();
-        if (includedFiles == null || includedFiles.length <= 0) {
-          return null;
+          // scanner only sees files with content changes since the last build
+          Scanner scanner = buildContext.newScanner(typeSystemFile);
+          scanner.scan();
+
+          // if no files have changed, don't run the Mojo
+          String[] includedFiles = scanner.getIncludedFiles();
+          if (includedFiles == null || includedFiles.length <= 0) {
+            return null;
+          }
         }
       }
     }
@@ -50,11 +59,6 @@ public class JCasGenBuildParticipant extends MojoExecutionBuildParticipant {
     Set<IProject> result = super.build(kind, monitor);
 
     // tell m2e builder to refresh generated files
-    File generatedSourcesDirectory = maven.getMojoParameterValue(
-        getSession(),
-        getMojoExecution(),
-        "outputDirectory",
-        File.class);
     if (generatedSourcesDirectory != null) {
       buildContext.refresh(generatedSourcesDirectory);
     }
