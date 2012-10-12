5a4602ebb459c79c1e39968fc29260fa8988bdf2
Lee Becker <lee.becker@gmail.com>
2012-10-11 23:56:35 -0600

    Added test to exercise the extraction with the MutualInformationFeatureSelectionExtractor

--- cleartk-ml/src/test/java/org/cleartk/classifier/feature/selection/MutualInformationTest.java
+++ cleartk-ml/src/test/java/org/cleartk/classifier/feature/selection/MutualInformationTest.java
@@ -23,10 +23,34 @@
  */
 package org.cleartk.classifier.feature.selection;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.Instance;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Bag;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Covered;
+import org.cleartk.classifier.feature.extractor.simple.CoveredTextExtractor;
 import org.cleartk.classifier.feature.selection.MutualInformationFeatureSelectionExtractor.MutualInformationStats;
 import org.cleartk.test.DefaultTestBase;
+import org.cleartk.type.test.Sentence;
+import org.cleartk.type.test.Token;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.uimafit.component.NoOpAnnotator;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.util.JCasUtil;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 
 /**
  * <br>
@@ -38,10 +62,18 @@ import org.junit.Test;
  */
 public class MutualInformationTest extends DefaultTestBase {
 
+  @SuppressWarnings("deprecation")
+  @Before
+  public void localSetUp() throws Throwable {
+    this.engine = AnalysisEngineFactory.createPrimitive(NoOpAnnotator.class, typeSystemDescription);
+    this.jCasObjects = new ArrayList<JCas>();
+  }
+
   @Test
   public void testMutualInformationStats() {
-    MutualInformationStats<String> stats = new MutualInformationStats<String>();
-    stats.update("export", "poultry", 49);
+    MutualInformationStats<String> stats = new MutualInformationStats<String>(0.0);
+    stats.update("export", "poultry", 25);
+    stats.update("export", "poultry", 24);
     stats.update("export", "not_poultry", 141);
     stats.update("not_export", "poultry", 27652);
     stats.update("not_export", "not_poultry", 774106);
@@ -54,10 +86,105 @@ public class MutualInformationTest extends DefaultTestBase {
     Assert.assertEquals(stats.classCounts.count("poultry"), 27701);
     Assert.assertEquals(stats.classCounts.count("not_poultry"), 774247);
 
-    Assert.assertEquals(stats.mutualInformation("export", "poultry"), 0.0001105, 0.00000005);
-    Assert.assertEquals(stats.mutualInformation("export", "not_poultry"), 0.0001105, 0.00000005);
-    Assert.assertEquals(stats.mutualInformation("not_export", "poultry"), 0.0001105, 0.00000005);
-    Assert.assertEquals(stats.mutualInformation("not_export", "not_poultry"), 0.0001105, 0.00000005);
+    Assert.assertEquals(stats.mutualInformation("export", "poultry"), 0.0000766, 0.00000005);
+    Assert.assertEquals(stats.mutualInformation("export", "not_poultry"), 0.0000766, 0.00000005);
+    Assert.assertEquals(stats.mutualInformation("not_export", "poultry"), 0.0000766, 0.00000005);
+    Assert.assertEquals(stats.mutualInformation("not_export", "not_poultry"), 0.0000766, 0.00000005);
   }
+
+  @Test
+  public void testMutualInformationFeatureSelection() throws UIMAException, IOException {
+
+    CleartkExtractor extractor = new CleartkExtractor(
+        Token.class,
+        new CoveredTextExtractor(),
+        new Bag(new Covered()));
+
+    MutualInformationFeatureSelectionExtractor<String> miExtractor = new MutualInformationFeatureSelectionExtractor<String>(
+        "miExtractor",
+        extractor,
+        5);
+
+    List<Instance<String>> instances = Lists.newArrayList();
+
+    this.jCasObjects = Arrays.asList(
+        this.buildJCas(CLASS_A_TEXT.toLowerCase(), "ClassA"),
+        this.buildJCas(CLASS_B_TEXT.toLowerCase(), "ClassB"),
+        this.buildJCas(CLASS_C_TEXT.toLowerCase(), "ClassC"));
+
+    for (JCas jcas : jCasObjects) {
+      JCas classView = jcas.getView("ClassView");
+      for (Sentence s : JCasUtil.select(jcas, Sentence.class)) {
+        Instance<String> inst = new Instance<String>(miExtractor.extract(jcas, s));
+        inst.setOutcome(classView.getDocumentText());
+        instances.add(inst);
+      }
+    }
+
+    // Train and save extractor model
+    miExtractor.train(instances);
+    File tmpDir = Files.createTempDir();
+    File miFile = new File(tmpDir, "mi.txt");
+    miExtractor.save(miFile.toURI());
+
+    // Load and check selected features
+    miExtractor.load(miFile.toURI());
+    List<String> selectedFeatures = miExtractor.getSelectedFeatures();
+    Assert.assertEquals(5, selectedFeatures.size());
+    Assert.assertEquals("Bag_Covered:monkey", selectedFeatures.get(0));
+    Assert.assertEquals("Bag_Covered:girl", selectedFeatures.get(1));
+    Assert.assertEquals("Bag_Covered:wolf", selectedFeatures.get(2));
+    Assert.assertEquals("Bag_Covered:pig", selectedFeatures.get(3));
+    Assert.assertEquals("Bag_Covered:boy", selectedFeatures.get(4));
+
+    // Extract features using selected features model
+    List<Feature> features = Lists.newArrayList();
+    for (Sentence s : JCasUtil.select(this.jCasObjects.get(0), Sentence.class)) {
+      features.addAll(miExtractor.extract(this.jCasObjects.get(0), s));
+    }
+
+    // Check that values are as specified
+    Assert.assertTrue(features.contains(new Feature("Bag_Covered", "boy")));
+    Assert.assertTrue(features.contains(new Feature("Bag_Covered", "pig")));
+    Assert.assertTrue(features.contains(new Feature("Bag_Covered", "wolf")));
+    Assert.assertFalse(features.contains(new Feature("Bag_Covered", "and")));
+    Assert.assertFalse(features.contains(new Feature("Bag_Covered", "the")));
+    Assert.assertFalse(features.contains(new Feature("Bag_Covered", ".")));
+    Assert.assertFalse(features.contains(new Feature("Bag_Covered", "girl")));
+  }
+
+  public List<Instance<String>> createInstances(int n, String featureName, String outcome) {
+    Instance<String> instance = new Instance<String>();
+    instance.add(new Feature(featureName));
+    instance.setOutcome(outcome);
+
+    List<Instance<String>> instances = Lists.newArrayList();
+    for (int i = 0; i < n; i++) {
+      instances.add(instance);
+    }
+    return instances;
+  }
+
+  private static final String CLASS_A_TEXT = "The boy and the cat . \n"
+      + "The boy and the dog . \n" + "The boy and the wolf . \n" + "The boy and the pig . \n";
+
+  private static final String CLASS_B_TEXT = "The girl and the cat . \n"
+      + "The girl and the dog . \n" + "The girl and the boy . \n" + "The girl and the chicken . \n";
+
+  private static final String CLASS_C_TEXT = "The monkey and the cat . \n"
+      + "The girl and the monkey . \n" + "The monkey and the boy . \n"
+      + "The monkey and the chicken . \n";
+
+  private JCas buildJCas(String text, String outcome) throws UIMAException {
+    JCas newJCas = this.engine.newJCas();
+    this.tokenBuilder.buildTokens(newJCas, text);
+    JCas classView = newJCas.createView("ClassView");
+    classView.setDocumentText(outcome);
+    return newJCas;
+  }
+
+  private AnalysisEngine engine;
+
+  private List<JCas> jCasObjects;
 
 }
