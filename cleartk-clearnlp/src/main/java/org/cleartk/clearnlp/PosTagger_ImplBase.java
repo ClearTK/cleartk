9930caf036e0c269443b003e71587cd60089d4c1
lee.becker@gmail.com <lee.becker@gmail.com>
2013-04-04 00:06:57 -0600

    Removed minor warnings.

--- cleartk-clearnlp/src/main/java/org/cleartk/clearnlp/PosTagger_ImplBase.java
+++ cleartk-clearnlp/src/main/java/org/cleartk/clearnlp/PosTagger_ImplBase.java
@@ -34,8 +34,6 @@ import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.cleartk.token.type.Sentence;
-import org.cleartk.token.type.Token;
 import org.uimafit.component.JCasAnnotator_ImplBase;
 import org.uimafit.descriptor.ConfigurationParameter;
 import org.uimafit.descriptor.TypeCapability;
