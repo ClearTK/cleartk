3606c40f2742fb5a2030394da3328f647d2c104b
Philip Ogren <philip@ogren.info>
2013-11-09 22:42:24 -0700

    added tests to support feature extraction tutorial

--- cleartk-ml/src/test/java/org/cleartk/classifier/feature/FeatureExtractionTutorialTest.java
+++ cleartk-ml/src/test/java/org/cleartk/classifier/feature/FeatureExtractionTutorialTest.java
@@ -34,6 +34,12 @@ import java.util.regex.Pattern;
 
 import org.apache.uima.fit.util.JCasUtil;
 import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Bag;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Focus;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Following;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Ngram;
+import org.cleartk.classifier.feature.extractor.CleartkExtractor.Preceding;
 import org.cleartk.classifier.feature.extractor.CoveredTextExtractor;
 import org.cleartk.classifier.feature.extractor.FeatureExtractor1;
 import org.cleartk.classifier.feature.extractor.TypePathExtractor;
@@ -42,7 +48,10 @@ import org.cleartk.classifier.feature.function.FeatureFunctionExtractor;
 import org.cleartk.classifier.feature.function.FeatureFunctionExtractor.BaseFeatures;
 import org.cleartk.classifier.feature.function.LowerCaseFeatureFunction;
 import org.cleartk.test.DefaultTestBase;
+import org.cleartk.type.test.Chunk;
+import org.cleartk.type.test.Lemma;
 import org.cleartk.type.test.Token;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -156,4 +165,154 @@ public class FeatureExtractionTutorialTest extends DefaultTestBase {
         features.get(1).getValue());
   }
 
+  // Get the text of the 2 tokens before a focus token
+  @Test
+  public void testCleartkExtractor1() throws Exception {
+    CleartkExtractor<Token, Token> extractor = new CleartkExtractor<Token, Token>(
+        Token.class,
+        new CoveredTextExtractor<Token>(),
+        new Preceding(2));
+    this.tokenBuilder.buildTokens(this.jCas, "The quick brown fox jumped over the lazy dog.");
+
+    Token fox = JCasUtil.selectByIndex(jCas, Token.class, 3);
+
+    List<Feature> features = extractor.extract(this.jCas, fox);
+    assertEquals(2, features.size());
+    assertEquals("Preceding_0_2_1", features.get(0).getName());
+    assertEquals("quick", features.get(0).getValue());
+    assertEquals("Preceding_0_2_0", features.get(1).getName());
+    assertEquals("brown", features.get(1).getValue());
+  }
+
+  // Get the part-of-speech tags of the 3 tokens after a focus annotation
+  @Test
+  public void testCleartkExtractor2() throws Exception {
+    CleartkExtractor<Chunk, Token> extractor = new CleartkExtractor<Chunk, Token>(
+        Token.class,
+        new TypePathExtractor<Token>(Token.class, "pos"),
+        new Following(3));
+
+    this.tokenBuilder.buildTokens(
+        this.jCas,
+        "The quick brown fox jumped over the lazy dog.",
+        "The quick brown fox jumped over the lazy dog .",
+        "DT JJ JJ NN VBD IN DT JJ NN .");
+
+    Chunk chunk = new Chunk(this.jCas, 20, 31);
+    chunk.addToIndexes();
+    Assert.assertEquals("jumped over", chunk.getCoveredText());
+
+    List<Feature> features = extractor.extract(this.jCas, chunk);
+    assertEquals(3, features.size());
+
+    assertEquals("Following_0_3_0_TypePath(Pos)", features.get(0).getName());
+    assertEquals("DT", features.get(0).getValue());
+    assertEquals("Following_0_3_1_TypePath(Pos)", features.get(1).getName());
+    assertEquals("JJ", features.get(1).getValue());
+    assertEquals("Following_0_3_2_TypePath(Pos)", features.get(2).getName());
+    assertEquals("NN", features.get(2).getValue());
+
+  }
+
+  // Get the tokens after a focus annotation, beginning 2 after and ending 5 after, as a bag of
+  // words
+
+  @Test
+  public void testCleartkExtractor3() throws Exception {
+    CleartkExtractor<Chunk, Token> extractor = new CleartkExtractor<Chunk, Token>(
+        Token.class,
+        new CoveredTextExtractor<Token>(),
+        new Bag(new Following(2, 5)));
+
+    this.tokenBuilder.buildTokens(this.jCas, "The quick brown fox jumped over the lazy dog .");
+
+    Chunk chunk = new Chunk(this.jCas, 4, 9);
+    chunk.addToIndexes();
+    Assert.assertEquals("quick", chunk.getCoveredText());
+
+    List<Feature> features = extractor.extract(this.jCas, chunk);
+    assertEquals(3, features.size());
+
+    assertEquals("Bag_Following_2_5", features.get(0).getName());
+    assertEquals("jumped", features.get(0).getValue());
+    assertEquals("Bag_Following_2_5", features.get(1).getName());
+    assertEquals("over", features.get(1).getValue());
+    assertEquals("Bag_Following_2_5", features.get(2).getName());
+    assertEquals("the", features.get(2).getValue());
+
+    // I considered adding the following description of the out-of-bounds case to the tutorial but
+    // decided it was too arcane and describes behavior that is perhaps not correct.
+    // If the specified range goes past the last token in the JCas, then "out-of-bounds" features
+    // will be generated. Such features have names whose prefix is {{{OOB}}} followed by a digit
+    // corresponding to how far out of the range the feature is. You may choose to filter out this
+    // features.
+
+    chunk = new Chunk(this.jCas, 20, 35);
+    chunk.addToIndexes();
+    Assert.assertEquals("jumped over the", chunk.getCoveredText());
+
+    features = extractor.extract(this.jCas, chunk);
+    assertEquals(3, features.size());
+
+    assertEquals("Bag_Following_2_5", features.get(0).getName());
+    assertEquals(".", features.get(0).getValue());
+    assertEquals("Bag_Following_2_5", features.get(1).getName());
+    assertEquals("OOB1", features.get(1).getValue());
+    assertEquals("Bag_Following_2_5", features.get(2).getName());
+    assertEquals("OOB2", features.get(2).getValue());
+
+  }
+
+  // Get an ngram concatenating the stem of the first word before a focus annotation and the first
+  // word contained in the focus annotation
+  @Test
+  public void testCleartkExtractor4() throws Exception {
+    CleartkExtractor<Token, Token> extractor = new CleartkExtractor<Token, Token>(
+        Token.class,
+        new TypePathExtractor<Token>(Token.class, "lemma/value"),
+        new Ngram(new Preceding(1), new Focus()));
+
+    this.tokenBuilder.buildTokens(
+        this.jCas,
+        "The quick brown fox jumped over the lazy dog.",
+        "The quick brown fox jumped over the lazy dog .",
+        "DT JJ JJ NN VBD IN DT JJ NN .");
+
+    // not a best practices lemmatizer!
+    JCasUtil.selectByIndex(this.jCas, Token.class, 0).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 0).getLemma().setValue("the");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 1).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 1).getLemma().setValue("quick");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 2).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 2).getLemma().setValue("brown");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 3).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 3).getLemma().setValue("fox");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 4).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 4).getLemma().setValue("jump");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 5).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 5).getLemma().setValue("over");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 6).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 6).getLemma().setValue("the");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 7).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 7).getLemma().setValue("lazy");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 8).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 8).getLemma().setValue("dog");
+    JCasUtil.selectByIndex(this.jCas, Token.class, 9).setLemma(new Lemma(jCas));
+    JCasUtil.selectByIndex(this.jCas, Token.class, 9).getLemma().setValue(".");
+
+    Token jumped = JCasUtil.selectByIndex(this.jCas, Token.class, 4);
+    Assert.assertEquals("jumped", jumped.getCoveredText());
+
+    List<Feature> features = extractor.extract(this.jCas, jumped);
+    assertEquals(1, features.size());
+
+    assertEquals("Ngram_Preceding_0_1_Focus_TypePath(LemmaValue)", features.get(0).getName());
+    assertEquals("fox_jump", features.get(0).getValue());
+
+  }
 }
+
+// for (Feature feature : features) {
+// System.out.println(feature.getName());
+// System.out.println(feature.getValue());
+// }
