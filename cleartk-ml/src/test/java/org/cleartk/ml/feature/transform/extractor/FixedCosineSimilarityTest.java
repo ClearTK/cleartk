ce27a5884d7fec11ab712d459c233e18362d1487
philip@ogren.info <philip@ogren.info>
2014-04-13 10:52:36 -0600

    added basic test for FixedCosineSimilarity

--- cleartk-ml/src/test/java/org/cleartk/ml/feature/transform/extractor/FixedCosineSimilarityTest.java
+++ cleartk-ml/src/test/java/org/cleartk/ml/feature/transform/extractor/FixedCosineSimilarityTest.java
@@ -39,7 +39,7 @@ import org.junit.Test;
  * 
  */
 
-public class CosineSimilarityTest extends DefaultTestBase {
+public class FixedCosineSimilarityTest extends DefaultTestBase {
 
   @Test
   public void testBasic() throws Exception {
@@ -56,18 +56,9 @@ public class CosineSimilarityTest extends DefaultTestBase {
     vector2.put("d", 4d);
     vector2.put("e", 6d);
 
-    // 2*2 + 3*3 + 5*4 = 33
-    assertEquals(33d, CosineSimilarity.dotProduct(vector1, vector2), 0.0001d);
-    // 2*2 + 3*3 + 4*4 + 5*5 =
-    assertEquals(54d, CosineSimilarity.dotProduct(vector1, vector1), 0.0001d);
-    // 2*2 + 3*3 + 4*4 + 6*6 =
-    assertEquals(65d, CosineSimilarity.dotProduct(vector2, vector2), 0.0001d);
+    SimilarityFunction similarity = new FixedCosineSimilarity(vector1);
+    assertEquals(0.557006652, similarity.distance(vector2, vector1), 0.0001d);
+    assertEquals(0.557006652, ((FixedCosineSimilarity) similarity).distance(vector2), 0.0001d);
 
-    // sqrt(54)
-    assertEquals(7.348469228349534d, CosineSimilarity.magnitude(vector1), 0.0001d);
-    // sqrt(65)
-    assertEquals(8.06225774829855d, CosineSimilarity.magnitude(vector2), 0.0001d);
-    // 33d / (7.348469228349534*8.06225774829855)
-    assertEquals(0.557006652, new CosineSimilarity().distance(vector1, vector2), 0.0001d);
   }
 }
