612b76d4c2139932b985331f1379bb2246399072
Tim Miller <tim.miller@gmail.com>
2014-07-04 20:59:19 -0400

    Issue-402: Added licenses to new source files.

--- cleartk-ml-tksvmlight/src/main/java/org/cleartk/ml/tksvmlight/kernel/SubsetTreeKernel.java
+++ cleartk-ml-tksvmlight/src/main/java/org/cleartk/ml/tksvmlight/kernel/SubsetTreeKernel.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright (c) 2007-2013, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
 package org.cleartk.ml.tksvmlight.kernel;
 
 import java.util.ArrayList;
@@ -11,6 +34,19 @@ import org.cleartk.util.treebank.TopTreebankNode;
 import org.cleartk.util.treebank.TreebankFormatParser;
 import org.cleartk.util.treebank.TreebankNode;
 
+/**
+ * <br>
+ * Copyright (c) 2007-2013, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * 
+ * <p>
+ * 
+ * This class implements the subset tree kernel defined by Collins and Duffy
+ * in multiple papers, but most notably "Convolution Kernels for Natural Language,"
+ * published at NIPS 2002.
+ * 
+ * @author Tim Miller
+ */
 
 public class SubsetTreeKernel implements TreeKernel {
   public static final double LAMBDA_DEFAULT = 0.4;
