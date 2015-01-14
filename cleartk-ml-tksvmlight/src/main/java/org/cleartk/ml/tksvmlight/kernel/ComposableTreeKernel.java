14e74cd5f97ea61e40b610331be56a255707ac5d
tmill <tim.miller@gmail.com>
2015-01-14 07:33:05 -0500

    Fixes Issue-406 (this last commit actually adds copyright info to new source files but there was no merge commit message otherwise).

--- cleartk-ml-tksvmlight/src/main/java/org/cleartk/ml/tksvmlight/kernel/ComposableTreeKernel.java
+++ cleartk-ml-tksvmlight/src/main/java/org/cleartk/ml/tksvmlight/kernel/ComposableTreeKernel.java
@@ -1,6 +1,43 @@
+/*
+ * Copyright (c) 2007-2013, Regents of the University of Colorado 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * Neither the name of the University of Colorado at Boulder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
 package org.cleartk.ml.tksvmlight.kernel;
 
 import org.cleartk.ml.tksvmlight.TreeFeature;
+
+/**
+ * 
+ * 
+ * <br>
+ * Copyright (c) 2015, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * 
+ * An interface for kernels which may be combined into larger kernel
+ * ensembles. Adds an additional simplified evaluate method that the kernel is required
+ * to implement.
+ * 
+ * @author Tim Miller
+ */
 
 public interface ComposableTreeKernel extends TreeKernel {
   public double evaluate(TreeFeature tf1, TreeFeature tf2);
